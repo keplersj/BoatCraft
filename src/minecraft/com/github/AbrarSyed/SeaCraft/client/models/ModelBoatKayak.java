76fc61c5615855c1d680f8b4b21f065ebb6657b9
AbrarSyed <sacabrarsyed@gmail.com>
2013-03-30 09:10:58 -0500

    Revert "Test"
    This reverts commit 8682565789672efc671a292e2aa4849d202ff10c.

--- src/minecraft/com/github/AbrarSyed/SeaCraft/client/models/ModelBoatKayak.java
+++ src/minecraft/com/github/AbrarSyed/SeaCraft/client/models/ModelBoatKayak.java
@@ -17,43 +17,56 @@ public class ModelBoatKayak extends ModelBase
 
 	public ModelBoatKayak()
 	{
-        boatSides[0] = new ModelRenderer(this, 0, 0);
-        boatSides[1] = new ModelRenderer(this, 0, 0);
-        boatSides[2] = new ModelRenderer(this, 0, 0);
-        boatSides[3] = new ModelRenderer(this, 0, 0);
-        boatSides[4] = new ModelRenderer(this, 0, 0);
+		/*
+		 * render = new NMTModelRenderer(this, "Kayak");
+		 * render.setTextureOffset(0, 0);
+		 * this.setTextureOffset("Kayak.all", 0, 0);
+		 * File file = new File("mods/"+SeaCraft.MODID+"/models/Kayak-Canoe.obj");
+		 * try
+		 * {
+		 * render.addModelOBJ("all", file.toURI().toURL().toString());
+		 * }
+		 * catch (MalformedURLException e)
+		 * {
+		 * e.printStackTrace();
+		 * }
+		 */
 
-        int length = 40;
-        int height = 6;
-        int thicknessFront = 12;
-        int thicknessBack = 10;
-        int thicknessSide = 1;
-        int width = 20;
-        int rotation = 4;
+		boatSides[0] = new ModelRenderer(this, 0, 8);
+		boatSides[1] = new ModelRenderer(this, 0, 0);
+		boatSides[2] = new ModelRenderer(this, 0, 0);
+		boatSides[3] = new ModelRenderer(this, 0, 0);
+		boatSides[4] = new ModelRenderer(this, 0, 0);
 
-        boatSides[0].addBox(-length / 2, -width / 2 + 2, -3.0F, length, width - 4, 4, 0.0F);
-        boatSides[0].setRotationPoint(0.0F, rotation, 0.0F);
+		int b0 = 24;
+		int b1 = 6;
+		int b2 = 20;
+		int b3 = 4;
 
-        // front
-        boatSides[1].addBox(-width / 2, -height - 1, -thicknessFront, width, height, thicknessFront + 1, 0.0F);
-        boatSides[1].setRotationPoint(-length / 2 + 1, rotation, 0.0F);
+		boatSides[0].addBox(-b0 / 2, -b2 / 2 + 2, -3.0F, b0, b2 - 4, 4, 0.0F);
+		boatSides[0].setRotationPoint(0.0F, b3, 0.0F);
 
-        // back
-        boatSides[2].addBox(-width / 2, -height - 1, -thicknessBack, width, height, thicknessBack + 1, 0.0F);
-        boatSides[2].setRotationPoint(length / 2 - 1, rotation, 0.0F);
+		// front
+		boatSides[1].addBox(-b0 / 2 + 2, -b1 - 1, -1.0F, b0 - 4, b1, 2, 0.0F);
+		boatSides[1].setRotationPoint(-b0 / 2 + 1, b3, 0.0F);
 
-        // left
-        boatSides[3].addBox(-length / 2 + 2, -height - 1, -thicknessSide, length - 4, height, thicknessSide + 1, 0.0F);
-        boatSides[3].setRotationPoint(0.0F, rotation, -width / 2 + 1);
+		// right
+		boatSides[2].addBox(-b0 / 2 + 2, -b1 - 1, -1.0F, b0 - 4, b1, 2, 0.0F);
+		boatSides[2].setRotationPoint(b0 / 2 - 1, b3, 0.0F);
 
-        // back
-        boatSides[4].addBox(-length / 2 + 2, -height - 1, -thicknessSide, length - 4, height, thicknessSide + 1, 0.0F);
-        boatSides[4].setRotationPoint(0.0F, rotation, width / 2 - 1);
+		// left
+		boatSides[3].addBox(-b0 / 2 + 2, -b1 - 1, -1.0F, b0 - 4, b1, 2, 0.0F);
+		boatSides[3].setRotationPoint(0.0F, b3, -b2 / 2 + 1);
 
-        boatSides[0].rotateAngleX = (float) Math.PI / 2F;
-        boatSides[1].rotateAngleY = (float) Math.PI * 3F / 2F;
-        boatSides[2].rotateAngleY = (float) Math.PI / 2F;
-        boatSides[3].rotateAngleY = (float) Math.PI;
+		// back
+		boatSides[4].addBox(-b0 / 2 + 2, -b1 - 1, -1.0F, b0 - 4, b1, 2, 0.0F);
+		boatSides[4].setRotationPoint(0.0F, b3, b2 / 2 - 1);
+
+		boatSides[0].rotateAngleX = (float) Math.PI / 2F;
+		boatSides[1].rotateAngleY = (float) Math.PI * 3F / 2F;
+		boatSides[2].rotateAngleY = (float) Math.PI / 2F;
+		boatSides[3].rotateAngleY = (float) Math.PI;
+
 	}
 
 	/**
