fedbf94ad333c5bfb0de22ae94b3f22e6bbe24ff
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-02 14:02:41 -0600

    Timer caught me! Broken unfinished code. Trying to find an efficient way to render blocks inside Boats.

--- common/k2b6s9j/BoatCraft/render/RenderCustomBoat.java
+++ common/k2b6s9j/BoatCraft/render/RenderCustomBoat.java
@@ -1,11 +1,15 @@
 package k2b6s9j.BoatCraft.render;
 
 import k2b6s9j.BoatCraft.entity.item.EntityCustomBoat;
+import net.minecraft.block.Block;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelBoat;
+import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.entity.RenderBoat;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
 
@@ -15,11 +19,14 @@ public class RenderCustomBoat extends RenderBoat {
 
     /** instance of ModelBoat for rendering */
     protected ModelBase modelBoat;
+    
+    protected final RenderBlocks renderBlocks;
 
     public RenderCustomBoat()
     {
         this.shadowSize = 0.5F;
         this.modelBoat = new ModelBoat();
+        this.renderBlocks = new RenderBlocks();
     }
 
     /**
@@ -47,6 +54,24 @@ public class RenderCustomBoat extends RenderBoat {
         GL11.glScalef(f4, f4, f4);
         GL11.glScalef(1.0F / f4, 1.0F / f4, 1.0F / f4);
         this.func_110777_b(par1EntityBoat);
+        
+        int j = par1EntityBoat.getDisplayTileOffset();
+        Block block = par1EntityBoat.getDisplayTile();
+        int k = par1EntityBoat.getDisplayTileData();
+
+        if (block != null)
+        {
+            GL11.glPushMatrix();
+            this.func_110776_a(TextureMap.field_110575_b);
+            float f8 = 0.75F;
+            GL11.glScalef(f8, f8, f8);
+            GL11.glTranslatef(0.0F, (float)j / 16.0F, 0.0F);
+            this.renderBlockInBoat(par1EntityBoat, par9, block, k);
+            GL11.glPopMatrix();
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            this.func_110777_b(par1EntityBoat);
+        }
+        
         GL11.glScalef(-1.0F, -1.0F, 1.0F);
         this.modelBoat.render(par1EntityBoat, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
         GL11.glPopMatrix();
@@ -71,5 +96,23 @@ public class RenderCustomBoat extends RenderBoat {
     public void doRender(Entity par1Entity, double par2, double par4, double par6, float par8, float par9)
     {
         this.renderBoat((EntityCustomBoat)par1Entity, par2, par4, par6, par8, par9);
+    }
+    
+    /**
+     * Renders the block that is inside the boat.
+     */
+    protected void renderBlockInBoat(EntityBoat entityBoat, float par2, Block par3Block, int par4)
+    {
+        // TODO Look into boat brightness later
+    	//float f1 = entityBoat.getBrightness(par2);
+        GL11.glPushMatrix();
+        //this.renderBlocks.renderBlockAsItem(par3Block, par4, f1);
+        this.renderBlocks.renderBlockAsItem(par3Block, par4, 0);
+        GL11.glPopMatrix();
+    }
+    
+    protected void renderBlockInBoat(Class <? extends EntityBoat> entityBoat, float par2, Block par3Block, int par4)
+    {
+    	
     }
 }
