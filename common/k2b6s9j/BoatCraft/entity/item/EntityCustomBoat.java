68f7c62dbe8875d116614d384040ff70a9ccb38b
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-01 20:40:57 -0600

    Each different material type will have a different "material ID" which you will register with an API method Coming soon!

--- common/k2b6s9j/BoatCraft/entity/item/EntityCustomBoat.java
+++ common/k2b6s9j/BoatCraft/entity/item/EntityCustomBoat.java
@@ -5,7 +5,7 @@ import net.minecraft.world.World;
 
 public class EntityCustomBoat extends EntityBoat {
 
-	public EntityCustomBoat(World world, double par2, double par4, double par6) {
+	public EntityCustomBoat(World world, double par2, double par4, double par6, int typeID) {
 		super(world, par2, par4, par6);
 	}
 
