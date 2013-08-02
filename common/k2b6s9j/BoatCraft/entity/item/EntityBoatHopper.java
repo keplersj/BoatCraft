a39f1371ec527b792ef5828e69bade79e90fe94b
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-02 13:45:27 -0600

    Flushed out the Hopper Boat. This should make for some interesting sorting systems.

--- common/k2b6s9j/BoatCraft/entity/item/EntityBoatHopper.java
+++ common/k2b6s9j/BoatCraft/entity/item/EntityBoatHopper.java
@@ -1,9 +1,21 @@
 package k2b6s9j.BoatCraft.entity.item;
 
+import java.util.List;
+import java.util.logging.Level;
+
+import net.minecraft.block.Block;
+import net.minecraft.command.IEntitySelector;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.Hopper;
+import net.minecraft.tileentity.TileEntityHopper;
 import net.minecraft.world.World;
+import cpw.mods.fml.common.FMLLog;
 
 public class EntityBoatHopper extends EntityBoatContainer implements Hopper {
+	
+	private int transferTicker = -1;
 	
 	public EntityBoatHopper(World par1World)
     {
@@ -15,40 +27,152 @@ public class EntityBoatHopper extends EntityBoatContainer implements Hopper {
         super(par1World, par2, par4, par6);
     }
 
-	@Override
-	public World getWorldObj() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public int getBoatType()
+    {
+        return 5;
+    }
 
-	@Override
-	public double getXPos() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public Block getDefaultDisplayTile()
+    {
+        return Block.hopperBlock;
+    }
 
-	@Override
-	public double getYPos() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public int getDefaultDisplayTileOffset()
+    {
+        return 1;
+    }
 
-	@Override
-	public double getZPos() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    /**
+     * Returns the number of slots in the inventory.
+     */
+    public int getSizeInventory()
+    {
+        return 5;
+    }
 
-	@Override
-	public int getSizeInventory() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public boolean func_130002_c(EntityPlayer par1EntityPlayer)
+    {
+        if (!this.worldObj.isRemote)
+        {
+            //par1EntityPlayer.displayGUIHopper(this);
+        	FMLLog.log(Level.INFO, "A Hopper GUI should really be showing by now, I'll fix it later.");
+        }
+
+        return true;
+    }
+    
+    /**
+     * Returns the worldObj for this tileEntity.
+     */
+    public World getWorldObj()
+    {
+        return this.worldObj;
+    }
+
+    /**
+     * Gets the world X position for this hopper entity.
+     */
+    public double getXPos()
+    {
+        return this.posX;
+    }
+
+    /**
+     * Gets the world Y position for this hopper entity.
+     */
+    public double getYPos()
+    {
+        return this.posY;
+    }
+
+    /**
+     * Gets the world Z position for this hopper entity.
+     */
+    public double getZPos()
+    {
+        return this.posZ;
+    }
+
+    /**
+     * Called to update the entity's position/logic.
+     */
+    public void onUpdate()
+    {
+        super.onUpdate();
+
+        if (!this.worldObj.isRemote && this.isEntityAlive() )
+        {
+            --this.transferTicker;
+
+            if (!this.canTransfer())
+            {
+                this.setTransferTicker(0);
+
+                if (this.func_96112_aD())
+                {
+                    this.setTransferTicker(4);
+                    this.onInventoryChanged();
+                }
+            }
+        }
+    }
+
+    public boolean func_96112_aD()
+    {
+        if (TileEntityHopper.suckItemsIntoHopper(this))
+        {
+            return true;
+        }
+        else
+        {
+            List list = this.worldObj.selectEntitiesWithinAABB(EntityItem.class, this.boundingBox.expand(0.25D, 0.0D, 0.25D), IEntitySelector.selectAnything);
+
+            if (list.size() > 0)
+            {
+                TileEntityHopper.func_96114_a(this, (EntityItem)list.get(0));
+            }
+
+            return false;
+        }
+    }
+    
+    /**
+     * (abstract) Protected helper method to write subclass entity data to NBT.
+     */
+    protected void writeEntityToNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.writeEntityToNBT(par1NBTTagCompound);
+        par1NBTTagCompound.setInteger("TransferCooldown", this.transferTicker);
+    }
+
+    /**
+     * (abstract) Protected helper method to read subclass entity data from NBT.
+     */
+    protected void readEntityFromNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.readEntityFromNBT(par1NBTTagCompound);
+        this.transferTicker = par1NBTTagCompound.getInteger("TransferCooldown");
+    }
+
+    /**
+     * Sets the transfer ticker, used to determine the delay between transfers.
+     */
+    public void setTransferTicker(int par1)
+    {
+        this.transferTicker = par1;
+    }
+
+    /**
+     * Returns whether the hopper cart can currently transfer an item.
+     */
+    public boolean canTransfer()
+    {
+        return this.transferTicker > 0;
+    }
 
 	@Override
 	public boolean isInvNameLocalized() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
 }
