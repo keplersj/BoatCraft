0097ce8ba7d8a3e1a980463a9329b417d1eed4a4
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-02 19:14:44 -0600

    The items now interact with their respective entities

--- common/k2b6s9j/BoatCraft/item/boat/BoatBirch.java
+++ common/k2b6s9j/BoatCraft/item/boat/BoatBirch.java
@@ -2,7 +2,7 @@ package k2b6s9j.BoatCraft.item.boat;
 
 import java.util.List;
 
-import k2b6s9j.BoatCraft.entity.item.EntityCustomBoat;
+import k2b6s9j.BoatCraft.entity.item.EntityBirchWoodBoat;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
@@ -12,7 +12,6 @@ import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.EnumMovingObjectType;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -93,7 +92,7 @@ public class BoatBirch extends ItemBoat {
                         --j;
                     }
 
-                    EntityCustomBoat entityboat = new EntityCustomBoat(par2World, (double)((float)i + 0.5F), (double)((float)j + 1.0F), (double)((float)k + 0.5F), new ResourceLocation("boatcraft/textures/boat/birch.png"));
+                    EntityBirchWoodBoat entityboat = new EntityBirchWoodBoat(par2World, (double)((float)i + 0.5F), (double)((float)j + 1.0F), (double)((float)k + 0.5F));
                     entityboat.rotationYaw = (float)(((MathHelper.floor_double((double)(par3EntityPlayer.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3) - 1) * 90);
 
                     if (!par2World.getCollidingBoundingBoxes(entityboat, entityboat.boundingBox.expand(-0.1D, -0.1D, -0.1D)).isEmpty())
