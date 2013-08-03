c1a28dd982101007c99fb3168919f4de68ec3f45
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-03 15:53:51 -0600

    Register basic item renders

--- common/k2b6s9j/BoatCraft/proxy/ClientProxy.java
+++ common/k2b6s9j/BoatCraft/proxy/ClientProxy.java
@@ -8,7 +8,10 @@ import k2b6s9j.BoatCraft.entity.item.EntityBoatTNT;
 import k2b6s9j.BoatCraft.entity.item.EntityJungleWoodBoat;
 import k2b6s9j.BoatCraft.entity.item.EntityOakWoodBoat;
 import k2b6s9j.BoatCraft.entity.item.EntitySpruceWoodBoat;
+import k2b6s9j.BoatCraft.item.boat.BoatBirch;
+import k2b6s9j.BoatCraft.item.boat.BoatJungle;
 import k2b6s9j.BoatCraft.item.boat.BoatOak;
+import k2b6s9j.BoatCraft.item.boat.BoatSpruce;
 import k2b6s9j.BoatCraft.render.RenderBirchWoodBoat;
 import k2b6s9j.BoatCraft.render.RenderChestBoat;
 import k2b6s9j.BoatCraft.render.RenderFurnaceBoat;
@@ -37,10 +40,10 @@ public class ClientProxy extends CommonProxy {
 		RenderingRegistry.registerEntityRenderingHandler(EntityBoatHopper.class, new RenderHopperBoat());
 		
 		//Register Item Renderers
-		registerItemRenderers();
-	}
-	
-	public static void registerItemRenderers() {
+		MinecraftForgeClient.registerItemRenderer(BoatOak.ID, new RenderOakWoodBoat());
+		MinecraftForgeClient.registerItemRenderer(BoatSpruce.ID, new RenderSpruceWoodBoat());
+		MinecraftForgeClient.registerItemRenderer(BoatBirch.ID, new RenderBirchWoodBoat());
+		MinecraftForgeClient.registerItemRenderer(BoatJungle.ID, new RenderJungleWoodBoat());
 	}
 
 }
