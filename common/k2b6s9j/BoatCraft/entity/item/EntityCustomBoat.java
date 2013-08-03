e1b52defb9198837fd2cf562ad887c22a6ffb0ea
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-02 19:09:56 -0600

    Going to try separating the different woods into different entity classes

--- common/k2b6s9j/BoatCraft/entity/item/EntityCustomBoat.java
+++ common/k2b6s9j/BoatCraft/entity/item/EntityCustomBoat.java
@@ -7,8 +7,12 @@ import net.minecraft.world.World;
 
 public class EntityCustomBoat extends EntityBoat {
 	
-	public EntityCustomBoat(World world, double par2, double par4, double par6, ResourceLocation resource) {
+	public EntityCustomBoat(World par1World)
+    {
+		super(par1World);
+    }
+	
+	public EntityCustomBoat(World world, double par2, double par4, double par6) {
 		super(world, par2, par4, par6);
-		RenderCustomBoat.texture(this, resource);
 	}
 }
