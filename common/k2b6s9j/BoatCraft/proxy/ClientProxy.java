f9feac0c1339a23f73cead1c8ffb0db89c535c0c
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-03 02:14:23 -0600

    Structure!

--- common/k2b6s9j/BoatCraft/proxy/ClientProxy.java
+++ common/k2b6s9j/BoatCraft/proxy/ClientProxy.java
@@ -1,26 +1,38 @@
 package k2b6s9j.BoatCraft.proxy;
 
-import java.util.logging.Level;
-
 import k2b6s9j.BoatCraft.entity.item.EntityBirchWoodBoat;
+import k2b6s9j.BoatCraft.entity.item.EntityBoatChest;
+import k2b6s9j.BoatCraft.entity.item.EntityBoatFurnace;
+import k2b6s9j.BoatCraft.entity.item.EntityBoatHopper;
+import k2b6s9j.BoatCraft.entity.item.EntityBoatTNT;
 import k2b6s9j.BoatCraft.entity.item.EntityJungleWoodBoat;
 import k2b6s9j.BoatCraft.entity.item.EntityOakWoodBoat;
 import k2b6s9j.BoatCraft.entity.item.EntitySpruceWoodBoat;
 import k2b6s9j.BoatCraft.render.RenderBirchWoodBoat;
+import k2b6s9j.BoatCraft.render.RenderChestBoat;
+import k2b6s9j.BoatCraft.render.RenderFurnaceBoat;
+import k2b6s9j.BoatCraft.render.RenderHopperBoat;
 import k2b6s9j.BoatCraft.render.RenderJungleWoodBoat;
 import k2b6s9j.BoatCraft.render.RenderOakWoodBoat;
 import k2b6s9j.BoatCraft.render.RenderSpruceWoodBoat;
+import k2b6s9j.BoatCraft.render.RenderTNTBoat;
 import cpw.mods.fml.client.registry.RenderingRegistry;
-import cpw.mods.fml.common.FMLLog;
 
 
 public class ClientProxy extends CommonProxy {
 	
 	public static void registerRenderers() {
+		//Regular Boats
 		RenderingRegistry.registerEntityRenderingHandler(EntityOakWoodBoat.class, new RenderOakWoodBoat());
 		RenderingRegistry.registerEntityRenderingHandler(EntitySpruceWoodBoat.class, new RenderSpruceWoodBoat());
 		RenderingRegistry.registerEntityRenderingHandler(EntityBirchWoodBoat.class, new RenderBirchWoodBoat());
 		RenderingRegistry.registerEntityRenderingHandler(EntityJungleWoodBoat.class, new RenderJungleWoodBoat());
+		
+		//Special Boats
+		RenderingRegistry.registerEntityRenderingHandler(EntityBoatChest.class, new RenderChestBoat());
+		RenderingRegistry.registerEntityRenderingHandler(EntityBoatFurnace.class, new RenderFurnaceBoat());
+		RenderingRegistry.registerEntityRenderingHandler(EntityBoatTNT.class, new RenderTNTBoat());
+		RenderingRegistry.registerEntityRenderingHandler(EntityBoatHopper.class, new RenderHopperBoat());
 	}
 
 }
---
+++ common/k2b6s9j/BoatCraft/render/RenderHopperBoat.java
@@ -0,0 +1,22 @@
+package k2b6s9j.BoatCraft.render;
+
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderHopperBoat extends Render {
+
+	@Override
+	public void doRender(Entity entity, double d0, double d1, double d2,
+			float f, float f1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected ResourceLocation func_110775_a(Entity entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
