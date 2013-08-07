a4f2341b3866733dce6030bc54c23919a8c0cb20
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-07 00:39:16 -0600

    Because mounting the buoy would be stupid.

--- common/k2b6s9j/BoatCraft/entity/item/EntityBuoy.java
+++ common/k2b6s9j/BoatCraft/entity/item/EntityBuoy.java
@@ -1,6 +1,7 @@
 package k2b6s9j.BoatCraft.entity.item;
 
 import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
 public class EntityBuoy extends EntityBoat {
@@ -13,6 +14,12 @@ public class EntityBuoy extends EntityBoat {
     public EntityBuoy(World par1World, double par2, double par4, double par6)
     {
         super(par1World, par2, par4, par6);
+    }
+    
+    @Override
+    public boolean func_130002_c(EntityPlayer player)
+    {
+    	return false;
     }
 
 }
