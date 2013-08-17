618b730d39991963374aa14b0a1738adcf30bfb6
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-17 01:38:00 -0600

    Fix compilation errors

--- common/k2b6s9j/BoatCraft/entity/boat/vanilla/wood/EntitySpruce.java
+++ common/k2b6s9j/BoatCraft/entity/boat/vanilla/wood/EntitySpruce.java
@@ -3,11 +3,19 @@ package k2b6s9j.BoatCraft.entity.boat.vanilla.wood;
 import net.minecraft.world.World;
 import k2b6s9j.BoatCraft.entity.boat.EntityCustomBoat;
 
-public abstract class EntitySpruce extends EntityCustomBoat {
+public class EntitySpruce extends EntityCustomBoat {
 
 	public EntitySpruce(World par1World, double par2, double par4, double par6) {
 		super(par1World, par2, par4, par6);
-		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public int getSizeInventory() {
+		return 0;
+	}
+
+	@Override
+	public boolean isInvNameLocalized() {
+		return false;
+	}
 }
