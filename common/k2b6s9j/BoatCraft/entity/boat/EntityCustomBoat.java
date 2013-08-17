5006ae52ed96c94d42bd1d993f13adb4ecfd2b0c
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-17 01:32:38 -0600

    Merged Boat Container code into the main custom boat class. I hope I don't regret this...

--- common/k2b6s9j/BoatCraft/entity/boat/EntityCustomBoat.java
+++ common/k2b6s9j/BoatCraft/entity/boat/EntityCustomBoat.java
@@ -8,9 +8,13 @@ import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityBoat;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
@@ -19,9 +23,8 @@ import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class EntityCustomBoat extends EntityBoat
+public abstract class EntityCustomBoat extends EntityBoat implements IInventory
 {
-
     public boolean field_70279_a;
 	private double speedMultiplier;
 	private int boatPosRotationIncrements;
@@ -30,6 +33,10 @@ public class EntityCustomBoat extends EntityBoat
 	private double boatZ;
 	private double boatYaw;
 	private double boatPitch;
+	
+	//Container Boat Fields
+	private ItemStack[] boatContainerItems = new ItemStack[36];
+	private boolean dropContentsWhenDead = true;
 
 	public EntityCustomBoat(World par1World)
     {
@@ -399,6 +406,7 @@ public class EntityCustomBoat extends EntityBoat
     
     public void crashedDrops()
     {
+    	dropContents();
         int k;
 
         for (k = 0; k < 3; ++k)
@@ -431,6 +439,9 @@ public class EntityCustomBoat extends EntityBoat
         }
     }
 
+    /*
+     * This function is called with the Player action clicks on the Boat entity
+     */
     public boolean func_130002_c(EntityPlayer player)
     {
     	//Do not mount if the player is shift clicking
@@ -446,13 +457,21 @@ public class EntityCustomBoat extends EntityBoat
         {
             if (!this.worldObj.isRemote)
             {
-                player.mountEntity(this);
+            	if (!hasDisplayTile()) {
+            		player.mountEntity(this);
+            	}
+            	else {
+            		
+            	}
             }
 
             return true;
         }
     }
     
+    /*
+     * These classes below supply the information needed for Block in Boats
+     */
     public Block getDisplayTile()
     {
         if (!this.hasDisplayTile())
@@ -525,5 +544,205 @@ public class EntityCustomBoat extends EntityBoat
     public void func_70270_d(boolean par1)
     {
         this.field_70279_a = par1;
+    }
+    
+    /**
+     * Returns the stack in slot i
+     */
+    public ItemStack getStackInSlot(int par1)
+    {
+        return this.boatContainerItems[par1];
+    }
+    
+    /**
+     * Removes from an inventory slot (first arg) up to a specified number (second arg) of items and returns them in a
+     * new stack.
+     */
+    public ItemStack decrStackSize(int par1, int par2)
+    {
+        if (this.boatContainerItems[par1] != null)
+        {
+            ItemStack itemstack;
+
+            if (this.boatContainerItems[par1].stackSize <= par2)
+            {
+                itemstack = this.boatContainerItems[par1];
+                this.boatContainerItems[par1] = null;
+                return itemstack;
+            }
+            else
+            {
+                itemstack = this.boatContainerItems[par1].splitStack(par2);
+
+                if (this.boatContainerItems[par1].stackSize == 0)
+                {
+                    this.boatContainerItems[par1] = null;
+                }
+
+                return itemstack;
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /**
+     * When some containers are closed they call this on each slot, then drop whatever it returns as an EntityItem -
+     * like when you close a workbench GUI.
+     */
+    public ItemStack getStackInSlotOnClosing(int par1)
+    {
+        if (this.boatContainerItems[par1] != null)
+        {
+            ItemStack itemstack = this.boatContainerItems[par1];
+            this.boatContainerItems[par1] = null;
+            return itemstack;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /**
+     * Sets the given item stack to the specified slot in the inventory (can be crafting or armor sections).
+     */
+    public void setInventorySlotContents(int par1, ItemStack par2ItemStack)
+    {
+        this.boatContainerItems[par1] = par2ItemStack;
+
+        if (par2ItemStack != null && par2ItemStack.stackSize > this.getInventoryStackLimit())
+        {
+            par2ItemStack.stackSize = this.getInventoryStackLimit();
+        }
+    }
+
+    /**
+     * Called when an the contents of an Inventory change, usually
+     */
+    public void onInventoryChanged() {}
+
+    /**
+     * Do not make give this method the name canInteractWith because it clashes with Container
+     */
+    public boolean isUseableByPlayer(EntityPlayer par1EntityPlayer)
+    {
+        return this.isDead ? false : par1EntityPlayer.getDistanceSqToEntity(this) <= 64.0D;
+    }
+
+    public void openChest() {}
+
+    public void closeChest() {}
+
+    /**
+     * Returns true if automation is allowed to insert the given stack (ignoring stack size) into the given slot.
+     */
+    public boolean isItemValidForSlot(int par1, ItemStack par2ItemStack)
+    {
+        return true;
+    }
+
+    /**
+     * Returns the name of the inventory.
+     */
+    public String getInvName()
+    {
+        return "container.boat";
+    }
+
+    /**
+     * Returns the maximum stack size for a inventory slot. Seems to always be 64, possibly will be extended. *Isn't
+     * this more of a set than a get?*
+     */
+    public int getInventoryStackLimit()
+    {
+        return 64;
+    }
+
+    /**
+     * Teleports the entity to another dimension. Params: Dimension number to teleport to
+     */
+    public void travelToDimension(int par1)
+    {
+        this.dropContentsWhenDead = false;
+        super.travelToDimension(par1);
+    }
+    
+    /**
+     * (abstract) Protected helper method to write subclass entity data to NBT.
+     */
+    public void writeEntityToNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.writeEntityToNBT(par1NBTTagCompound);
+        NBTTagList nbttaglist = new NBTTagList();
+
+        for (int i = 0; i < this.boatContainerItems.length; ++i)
+        {
+            if (this.boatContainerItems[i] != null)
+            {
+                NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+                nbttagcompound1.setByte("Slot", (byte)i);
+                this.boatContainerItems[i].writeToNBT(nbttagcompound1);
+                nbttaglist.appendTag(nbttagcompound1);
+            }
+        }
+
+        par1NBTTagCompound.setTag("Items", nbttaglist);
+    }
+
+    /**
+     * (abstract) Protected helper method to read subclass entity data from NBT.
+     */
+    public void readEntityFromNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.readEntityFromNBT(par1NBTTagCompound);
+        NBTTagList nbttaglist = par1NBTTagCompound.getTagList("Items");
+        this.boatContainerItems = new ItemStack[this.getSizeInventory()];
+
+        for (int i = 0; i < nbttaglist.tagCount(); ++i)
+        {
+            NBTTagCompound nbttagcompound1 = (NBTTagCompound)nbttaglist.tagAt(i);
+            int j = nbttagcompound1.getByte("Slot") & 255;
+
+            if (j >= 0 && j < this.boatContainerItems.length)
+            {
+                this.boatContainerItems[j] = ItemStack.loadItemStackFromNBT(nbttagcompound1);
+            }
+        }
+    }
+    
+    public void dropContents()
+    {
+    	for (int i = 0; i < this.getSizeInventory(); ++i)
+        {
+            ItemStack itemstack = this.getStackInSlot(i);
+
+            if (itemstack != null)
+            {
+                float f = this.rand.nextFloat() * 0.8F + 0.1F;
+                float f1 = this.rand.nextFloat() * 0.8F + 0.1F;
+                float f2 = this.rand.nextFloat() * 0.8F + 0.1F;
+
+                while (itemstack.stackSize > 0)
+                {
+                    int j = this.rand.nextInt(21) + 10;
+
+                    if (j > itemstack.stackSize)
+                    {
+                        j = itemstack.stackSize;
+                    }
+
+                    itemstack.stackSize -= j;
+                    EntityItem entityitem = new EntityItem(this.worldObj, this.posX + (double)f, this.posY + (double)f1, this.posZ + (double)f2, new ItemStack(itemstack.itemID, j, itemstack.getItemDamage()));
+                    float f3 = 0.05F;
+                    entityitem.motionX = (double)((float)this.rand.nextGaussian() * f3);
+                    entityitem.motionY = (double)((float)this.rand.nextGaussian() * f3 + 0.2F);
+                    entityitem.motionZ = (double)((float)this.rand.nextGaussian() * f3);
+                    this.worldObj.spawnEntityInWorld(entityitem);
+                }
+            }
+        }
     }
 }
