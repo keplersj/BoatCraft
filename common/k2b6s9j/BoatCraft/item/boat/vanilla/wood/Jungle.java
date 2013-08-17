e98ca81789a114955fc85f855666f896a3689bb2
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-08-17 01:12:58 -0600

    Not as clean as I dreamed Cleaner that what existed

--- common/k2b6s9j/BoatCraft/item/boat/vanilla/wood/Jungle.java
+++ common/k2b6s9j/BoatCraft/item/boat/vanilla/wood/Jungle.java
@@ -1,7 +1,11 @@
 package k2b6s9j.BoatCraft.item.boat.vanilla.wood;
 
+import k2b6s9j.BoatCraft.entity.boat.vanilla.wood.EntityJungle;
 import k2b6s9j.BoatCraft.item.boat.ItemCustomBoat;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
 import net.minecraftforge.oredict.OreDictionary;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -16,6 +20,27 @@ public class Jungle extends ItemCustomBoat {
         func_111206_d("boatcraft:boatJungle");
     	GameRegistry.registerItem(this, "Jungle Wood Boat");
     	OreDictionary.registerOre("itemBoat", new ItemStack(this));
+	}
+	
+	public ItemStack deployEntity(ItemStack itemStack, World world, EntityPlayer player, int x, int y, int z) {
+		EntityJungle entity = new EntityJungle(world, (double)((float)x + 0.5F), (double)((float)y + 1.0F), (double)((float)z + 0.5F));
+        entity.rotationYaw = (float)(((MathHelper.floor_double((double)(player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3) - 1) * 90);
+        
+        if (!world.getCollidingBoundingBoxes(entity, entity.boundingBox.expand(-0.1D, -0.1D, -0.1D)).isEmpty())
+        {
+            return itemStack;
+        }
+
+        if (!world.isRemote)
+        {
+            world.spawnEntityInWorld(entity);
+        }
+
+        if (!player.capabilities.isCreativeMode)
+        {
+            --itemStack.stackSize;
+        }
+        return itemStack;
 	}
 
 }
