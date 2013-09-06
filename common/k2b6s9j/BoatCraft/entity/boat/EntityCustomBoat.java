1ff23396061077aa894dff016ab5bf62918934f9
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-09-05 23:41:00 -0600

    Cleaned out methods identical to EntityBoat

--- common/k2b6s9j/BoatCraft/entity/boat/EntityCustomBoat.java
+++ common/k2b6s9j/BoatCraft/entity/boat/EntityCustomBoat.java
@@ -46,47 +46,6 @@ public class EntityCustomBoat extends EntityBoat
         this.yOffset = this.height / 2.0F;
     }
 
-    /**
-     * returns if this entity triggers Block.onEntityWalking on the blocks they walk on. used for spiders and wolves to
-     * prevent them from trampling crops
-     */
-    protected boolean canTriggerWalking()
-    {
-        return false;
-    }
-
-    protected void entityInit()
-    {
-        this.dataWatcher.addObject(17, new Integer(0));
-        this.dataWatcher.addObject(18, new Integer(1));
-        this.dataWatcher.addObject(19, new Float(0.0F));
-    }
-
-    /**
-     * Returns a boundingBox used to collide the entity with other entities and blocks. This enables the entity to be
-     * pushable on contact, like boats or minecarts.
-     */
-    public AxisAlignedBB getCollisionBox(Entity par1Entity)
-    {
-        return par1Entity.boundingBox;
-    }
-
-    /**
-     * returns the bounding box for this entity
-     */
-    public AxisAlignedBB getBoundingBox()
-    {
-        return this.boundingBox;
-    }
-
-    /**
-     * Returns true if this entity should push and be pushed by other entities when colliding.
-     */
-    public boolean canBePushed()
-    {
-        return true;
-    }
-
     public EntityCustomBoat(World par1World, double par2, double par4, double par6)
     {
         this(par1World);
@@ -97,68 +56,6 @@ public class EntityCustomBoat extends EntityBoat
         this.prevPosX = par2;
         this.prevPosY = par4;
         this.prevPosZ = par6;
-    }
-
-    /**
-     * Returns the Y offset from the entity's position for any entity riding this one.
-     */
-    public double getMountedYOffset()
-    {
-        return (double)this.height * 0.0D - 0.30000001192092896D;
-    }
-    
-    /**
-     * Called when the entity is attacked.
-     */
-    public boolean attackEntityFrom(DamageSource par1DamageSource, float par2)
-    {
-        if (this.isEntityInvulnerable())
-        {
-            return false;
-        }
-        else if (!this.worldObj.isRemote && !this.isDead)
-        {
-            this.setForwardDirection(-this.getForwardDirection());
-            this.setTimeSinceHit(10);
-            this.setDamageTaken(this.getDamageTaken() + par2 * 10.0F);
-            this.setBeenAttacked();
-            boolean flag = par1DamageSource.getEntity() instanceof EntityPlayer && ((EntityPlayer)par1DamageSource.getEntity()).capabilities.isCreativeMode;
-
-            if (flag || this.getDamageTaken() > 40.0F)
-            {
-                if (this.riddenByEntity != null)
-                {
-                    this.riddenByEntity.mountEntity(this);
-                }
-
-                if (!flag)
-                {
-                	if (isCustomBoat())
-                	{
-                		if (useItemID())
-                		{
-                			this.dropItemWithOffset(customBoatItemID(), 1, 0.0F);
-                		}
-                		else
-                		{
-                			this.entityDropItem(customBoatItem(), 0.0F);
-                		}
-                	}
-                	else
-                	{
-                		this.dropItemWithOffset(BoatOak.shiftedID, 1, 0.0F);
-                	}
-                }
-
-                this.setDead();
-            }
-
-            return true;
-        }
-        else
-        {
-            return true;
-        }
     }
 
     public boolean isCustomBoat()
@@ -233,75 +130,6 @@ public class EntityCustomBoat extends EntityBoat
         {
         	this.entityDropItem(blockInBoat(), 0.0F);
         }
-    }
-    
-    @SideOnly(Side.CLIENT)
-
-    /**
-     * Setups the entity to do the hurt animation. Only used by packets in multiplayer.
-     */
-    public void performHurtAnimation()
-    {
-        this.setForwardDirection(-this.getForwardDirection());
-        this.setTimeSinceHit(10);
-        this.setDamageTaken(this.getDamageTaken() * 11.0F);
-    }
-
-    /**
-     * Returns true if other Entities should be prevented from moving through this Entity.
-     */
-    public boolean canBeCollidedWith()
-    {
-        return !this.isDead;
-    }
-
-    @SideOnly(Side.CLIENT)
-
-    /**
-     * Sets the position and rotation. Only difference from the other one is no bounding on the rotation. Args: posX,
-     * posY, posZ, yaw, pitch
-     */
-    public void setPositionAndRotation2(double par1, double par3, double par5, float par7, float par8, int par9)
-    {
-        if (this.field_70279_a)
-        {
-            this.boatPosRotationIncrements = par9 + 5;
-        }
-        else
-        {
-            double d3 = par1 - this.posX;
-            double d4 = par3 - this.posY;
-            double d5 = par5 - this.posZ;
-            double d6 = d3 * d3 + d4 * d4 + d5 * d5;
-
-            if (d6 <= 1.0D)
-            {
-                return;
-            }
-
-            this.boatPosRotationIncrements = 3;
-        }
-
-        this.boatX = par1;
-        this.boatY = par3;
-        this.boatZ = par5;
-        this.boatYaw = (double)par7;
-        this.boatPitch = (double)par8;
-        this.motionX = this.velocityX;
-        this.motionY = this.velocityY;
-        this.motionZ = this.velocityZ;
-    }
-
-    @SideOnly(Side.CLIENT)
-
-    /**
-     * Sets the velocity to the args. Args: x, y, z
-     */
-    public void setVelocity(double par1, double par3, double par5)
-    {
-        this.velocityX = this.motionX = par1;
-        this.velocityY = this.motionY = par3;
-        this.velocityZ = this.motionZ = par5;
     }
 
     /**
@@ -561,32 +389,6 @@ public class EntityCustomBoat extends EntityBoat
         }
     }
 
-    public void updateRiderPosition()
-    {
-        if (this.riddenByEntity != null)
-        {
-            double d0 = Math.cos((double)this.rotationYaw * Math.PI / 180.0D) * 0.4D;
-            double d1 = Math.sin((double)this.rotationYaw * Math.PI / 180.0D) * 0.4D;
-            this.riddenByEntity.setPosition(this.posX + d0, this.posY + this.getMountedYOffset() + this.riddenByEntity.getYOffset(), this.posZ + d1);
-        }
-    }
-
-    /**
-     * (abstract) Protected helper method to write subclass entity data to NBT.
-     */
-    protected void writeEntityToNBT(NBTTagCompound par1NBTTagCompound) {}
-
-    /**
-     * (abstract) Protected helper method to read subclass entity data from NBT.
-     */
-    protected void readEntityFromNBT(NBTTagCompound par1NBTTagCompound) {}
-
-    @SideOnly(Side.CLIENT)
-    public float getShadowSize()
-    {
-        return 0.0F;
-    }
-
     public boolean func_130002_c(EntityPlayer player)
     {
     	//Do not mount if the player is shift clicking
@@ -607,54 +409,6 @@ public class EntityCustomBoat extends EntityBoat
 
             return true;
         }
-    }
-
-    /**
-     * Sets the damage taken from the last hit.
-     */
-    public void setDamageTaken(float par1)
-    {
-        this.dataWatcher.updateObject(19, Float.valueOf(par1));
-    }
-
-    /**
-     * Gets the damage taken from the last hit.
-     */
-    public float getDamageTaken()
-    {
-        return this.dataWatcher.func_111145_d(19);
-    }
-
-    /**
-     * Sets the time to count down from since the last time entity was hit.
-     */
-    public void setTimeSinceHit(int par1)
-    {
-        this.dataWatcher.updateObject(17, Integer.valueOf(par1));
-    }
-
-    /**
-     * Gets the time since the last hit.
-     */
-    public int getTimeSinceHit()
-    {
-        return this.dataWatcher.getWatchableObjectInt(17);
-    }
-
-    /**
-     * Sets the forward direction of the entity.
-     */
-    public void setForwardDirection(int par1)
-    {
-        this.dataWatcher.updateObject(18, Integer.valueOf(par1));
-    }
-
-    /**
-     * Gets the forward direction of the entity.
-     */
-    public int getForwardDirection()
-    {
-        return this.dataWatcher.getWatchableObjectInt(18);
     }
     
     public Block getDisplayTile()
@@ -723,11 +477,5 @@ public class EntityCustomBoat extends EntityBoat
     public void setHasDisplayTile(boolean par1)
     {
         this.getDataWatcher().updateObject(22, Byte.valueOf((byte)(par1 ? 1 : 0)));
-    }
-    
-    @SideOnly(Side.CLIENT)
-    public void func_70270_d(boolean par1)
-    {
-        this.field_70279_a = par1;
     }
 }
