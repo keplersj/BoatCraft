8541191bf3b0a590535b65d9456a62d95fb859d2
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-09-05 23:46:14 -0600

    So many new entities That need so much work.

---
+++ common/k2b6s9j/BoatCraft/entity/boat/wood/spruce/EntityBoatSpruceFurnace.java
@@ -0,0 +1,69 @@
+package k2b6s9j.BoatCraft.entity.boat.wood.spruce;
+
+import k2b6s9j.BoatCraft.entity.boat.EntityCustomBoat;
+import k2b6s9j.BoatCraft.item.boat.wood.oak.BoatOakFurnace;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class EntityBoatSpruceFurnace extends EntityCustomBoat {
+	
+	public BoatOakFurnace item;
+	
+	public EntityBoatSpruceFurnace(World par1World)
+    {
+        super(par1World);
+    }
+
+    public EntityBoatSpruceFurnace(World par1World, double par2, double par4, double par6)
+    {
+        super(par1World, par2, par4, par6);
+    }
+    
+    @Override
+    public boolean doesBoatContainBlock()
+    {
+    	return true;
+    }
+    
+    @Override
+    public ItemStack blockInBoat()
+    {
+    	return new ItemStack(Block.furnaceIdle, 1, 0);
+    }
+
+    public Block getDefaultDisplayTile()
+    {
+        return Block.furnaceBurning;
+    }
+
+    public int getDefaultDisplayTileData()
+    {
+        return 2;
+    }
+    
+    public boolean func_130002_c(EntityPlayer player)
+    {
+    	//Do not mount this boat on right click!
+        return false;
+    }
+    
+    @Override
+	public boolean isCustomBoat()
+    {
+    	return true;
+    }
+    
+    @Override
+	public boolean useItemID()
+	{
+		return true;
+	}
+    
+	@Override
+    public int customBoatItemID()
+    {
+    	return item.shiftedID;
+    }
+}
