6f8eb38d26677d61a140d56f9da7bec22f9c2ab6
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-11-27 21:45:52 -0700

    Ported completely to scala

--- src/main/java/k2b6s9j/BoatCraft/registry/ConfigRegistry.scala
+++ src/main/java/k2b6s9j/BoatCraft/registry/ConfigRegistry.scala
@@ -1,57 +1,59 @@
-package k2b6s9j.BoatCraft.registry;
+package k2b6s9j.BoatCraft.registry
 
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import k2b6s9j.BoatCraft.item.boat.wood.birch.*;
-import k2b6s9j.BoatCraft.item.boat.wood.jungle.*;
-import k2b6s9j.BoatCraft.item.boat.wood.oak.*;
-import k2b6s9j.BoatCraft.item.boat.wood.spruce.*;
-import k2b6s9j.BoatCraft.utilities.log.ModLogger;
-import net.minecraftforge.common.Configuration;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent
 
-public class ConfigRegistry {
+import k2b6s9j.BoatCraft.utilities.log.ModLogger
+import net.minecraftforge.common.Configuration
+import k2b6s9j.BoatCraft.item.boat.wood.oak._
+import k2b6s9j.BoatCraft.item.boat.wood.spruce._
+import k2b6s9j.BoatCraft.item.boat.wood.birch._
+import k2b6s9j.BoatCraft.item.boat.wood.jungle._
+
+class ConfigRegistry {
 
     //Config File Strings
-    public static final String itemBoats = "Boats in Item Form";
+    def itemBoats: String = "Boats in Item Form"
 
-    public static boolean OreDictWoodBoat;
+    def OreDictWoodBoat:Boolean
 
-    public static void CreateConfig (FMLPreInitializationEvent event) {
-        Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
-        try
+  public static void CreateConfig (FMLPreInitializationEvent event) {
+        Configuration.cfg = new Configuration(event.getSuggestedConfigurationFile())
+    try
         {
             //Boats
-            BoatOak.ID = cfg.getItem(itemBoats, "Oak Boat", 25500).getInt(25500);
-            BoatOakChest.ID = cfg.getItem(itemBoats, "Oak Chest Boat", 25501).getInt(25501);
-            BoatOakFurnace.ID = cfg.getItem(itemBoats, "Oak Furnace Boat", 25502).getInt(25502);
-            BoatOakHopper.ID = cfg.getItem(itemBoats, "Oak Hopper Boat", 25503).getInt(25503);
-            BoatOakTNT.ID = cfg.getItem(itemBoats, "Oak TNT Boat", 25504).getInt(25504);
-            BoatSpruce.ID = cfg.getItem(itemBoats, "Spruce Boat", 25505).getInt(25505);
-            BoatSpruceChest.ID = cfg.getItem(itemBoats, "Spruce Chest Boat", 25506).getInt(25506);
-            BoatSpruceFurnace.ID = cfg.getItem(itemBoats, "Spruce Furnace Boat", 25507).getInt(25507);
-            BoatSpruceHopper.ID = cfg.getItem(itemBoats, "Spruce Hopper Boat", 25508).getInt(25508);
-            BoatSpruceTNT.ID = cfg.getItem(itemBoats, "Spruce TNT Boat", 25509).getInt(25509);
-            BoatBirch.ID = cfg.getItem(itemBoats, "Birch Boat", 25510).getInt(25510);
-            BoatBirchChest.ID = cfg.getItem(itemBoats, "Birch Chest Boat", 25511).getInt(25511);
-            BoatBirchFurnace.ID = cfg.getItem(itemBoats, "Birch Furnace Boat", 25512).getInt(25512);
-            BoatBirchHopper.ID = cfg.getItem(itemBoats, "Birch Hopper Boat", 25513).getInt(25513);
-            BoatBirchTNT.ID = cfg.getItem(itemBoats, "Birch TNT Boat", 25514).getInt(25514);
-            BoatJungle.ID = cfg.getItem(itemBoats, "Jungle Boat", 25515).getInt(25515);
-            BoatJungleChest.ID = cfg.getItem(itemBoats, "Jungle Chest Boat", 25516).getInt(25516);
-            BoatJungleFurnace.ID = cfg.getItem(itemBoats, "Jungle Furnace Boat", 25517).getInt(25517);
-            BoatJungleHopper.ID = cfg.getItem(itemBoats, "Jungle Hopper Boat", 25518).getInt(25518);
-            BoatJungleTNT.ID = cfg.getItem(itemBoats, "Jungle TNT Boat", 25519).getInt(25519);
+            BoatOak.ID = cfg.getItem(itemBoats, "Oak Boat", 25500).getInt(25500)
+            BoatOakChest.ID = cfg.getItem(itemBoats, "Oak Chest Boat", 25501).getInt(25501)
+            BoatOakFurnace.ID = cfg.getItem(itemBoats, "Oak Furnace Boat", 25502).getInt(25502)
+            BoatOakHopper.ID = cfg.getItem(itemBoats, "Oak Hopper Boat", 25503).getInt(25503)
+            BoatOakTNT.ID = cfg.getItem(itemBoats, "Oak TNT Boat", 25504).getInt(25504)
+            BoatSpruce.ID = cfg.getItem(itemBoats, "Spruce Boat", 25505).getInt(25505)
+            BoatSpruceChest.ID = cfg.getItem(itemBoats, "Spruce Chest Boat", 25506).getInt(25506)
+            BoatSpruceFurnace.ID = cfg.getItem(itemBoats, "Spruce Furnace Boat", 25507).getInt(25507)
+            BoatSpruceHopper.ID = cfg.getItem(itemBoats, "Spruce Hopper Boat", 25508).getInt(25508)
+            BoatSpruceTNT.ID = cfg.getItem(itemBoats, "Spruce TNT Boat", 25509).getInt(25509)
+            BoatBirch.ID = cfg.getItem(itemBoats, "Birch Boat", 25510).getInt(25510)
+            BoatBirchChest.ID = cfg.getItem(itemBoats, "Birch Chest Boat", 25511).getInt(25511)
+            BoatBirchFurnace.ID = cfg.getItem(itemBoats, "Birch Furnace Boat", 25512).getInt(25512)
+            BoatBirchHopper.ID = cfg.getItem(itemBoats, "Birch Hopper Boat", 25513).getInt(25513)
+            BoatBirchTNT.ID = cfg.getItem(itemBoats, "Birch TNT Boat", 25514).getInt(25514)
+            BoatJungle.ID = cfg.getItem(itemBoats, "Jungle Boat", 25515).getInt(25515)
+            BoatJungleChest.ID = cfg.getItem(itemBoats, "Jungle Chest Boat", 25516).getInt(25516)
+            BoatJungleFurnace.ID = cfg.getItem(itemBoats, "Jungle Furnace Boat", 25517).getInt(25517)
+            BoatJungleHopper.ID = cfg.getItem(itemBoats, "Jungle Hopper Boat", 25518).getInt(25518)
+            BoatJungleTNT.ID = cfg.getItem(itemBoats, "Jungle TNT Boat", 25519).getInt(25519)
 
             //Modules
-            OreDictWoodBoat = cfg.get("Modules", "OreDictWoodBoats", false, "Use the OreDictionary to craft Wooden Boats").getBoolean(false);
-        }
-        catch (Exception e)
+            OreDictWoodBoat = cfg.get("Modules", "OreDictWoodBoats", false, "Use the OreDictionary to craft Wooden Boats").getBoolean(false)
+    }
+        catch
         {
-            ModLogger.severe("Failed to load configuration file.");
+          case e: Exception => ModLogger.severe("Failed to load configuration file.")
+          case e: Exception => e.printStackTrace()
         }
         finally
         {
             if (cfg.hasChanged())
-                cfg.save();
+                cfg.save()
         }
     }
 }
--- src/main/java/k2b6s9j/BoatCraft/registry/EntityRegistry.scala
+++ src/main/java/k2b6s9j/BoatCraft/registry/EntityRegistry.scala
@@ -1,36 +1,36 @@
-package k2b6s9j.BoatCraft.registry;
+package k2b6s9j.BoatCraft.registry
 
-import k2b6s9j.BoatCraft.BoatCraft;
-import k2b6s9j.BoatCraft.entity.boat.wood.birch.*;
-import k2b6s9j.BoatCraft.entity.boat.wood.jungle.*;
-import k2b6s9j.BoatCraft.entity.boat.wood.oak.*;
-import k2b6s9j.BoatCraft.entity.boat.wood.spruce.*;
+import k2b6s9j.BoatCraft.BoatCraft
+import k2b6s9j.BoatCraft.entity.boat.wood.birch._
+import k2b6s9j.BoatCraft.entity.boat.wood.jungle._
+import k2b6s9j.BoatCraft.entity.boat.wood.oak._
+import k2b6s9j.BoatCraft.entity.boat.wood.spruce._
 
-public class EntityRegistry {
+class EntityRegistry {
 
-    private static BoatCraft mod;
+    def mod:BoatCraft
 
     public static void RegisterEntities() {
-        int id = 1;
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatOak.class, "Oak Wood Boat", id, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatOakChest.class, "Oak Wood Chest Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatOakFurnace.class, "Oak Wood Furnace Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatOakHopper.class, "Oak Wood Hopper Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatOakTNT.class, "Oak Wood TNT Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatSpruce.class, "Spruce Wood Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatSpruceChest.class, "Spruce Wood Chest Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatSpruceFurnace.class, "Spruce Wood Furnace Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatSpruceHopper.class, "Spruce Wood Hopper Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatSpruceTNT.class, "Spruce Wood TNT Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatBirch.class, "Birch Wood Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatBirchChest.class, "Birch Wood Chest Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatBirchFurnace.class, "Birch Wood Furnace Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatBirchHopper.class, "Birch Wood Hopper Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatBirchTNT.class, "Birch Wood TNT Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatJungle.class, "Jungle Wood Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatJungleChest.class, "Jungle Wood Chest Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatJungleFurnace.class, "Jungle Wood Furnace Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatJungleHopper.class, "Jungle Wood Hopper Boat", id++, mod, 80, 3, true);
-        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatJungleTNT.class, "Jungle Wood TNT Boat", id++, mod, 80, 3, true);
+        int.id = 1
+        cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatOak, "Oak Wood Boat", id, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatOakChest, "Oak Wood Chest Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatOakFurnace, "Oak Wood Furnace Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatOakHopper, "Oak Wood Hopper Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatOakTNT, "Oak Wood TNT Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatSpruce, "Spruce Wood Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatSpruceChest, "Spruce Wood Chest Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatSpruceFurnace, "Spruce Wood Furnace Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatSpruceHopper, "Spruce Wood Hopper Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatSpruceTNT, "Spruce Wood TNT Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatBirch, "Birch Wood Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatBirchChest, "Birch Wood Chest Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatBirchFurnace, "Birch Wood Furnace Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatBirchHopper, "Birch Wood Hopper Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatBirchTNT, "Birch Wood TNT Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatJungle, "Jungle Wood Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatJungleChest, "Jungle Wood Chest Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatJungleFurnace, "Jungle Wood Furnace Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatJungleHopper, "Jungle Wood Hopper Boat", id++, mod, 80, 3, true)
+      cpw.mods.fml.common.registry.EntityRegistry.registerModEntity(EntityBoatJungleTNT, "Jungle Wood TNT Boat", id++, mod, 80, 3, true)
     }
 }
