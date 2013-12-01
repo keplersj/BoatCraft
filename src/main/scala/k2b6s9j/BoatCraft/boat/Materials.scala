245ff8c2bd81df1eba954bcc1ba8704232ba0d99
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-01 15:12:38 -0700

    Material Traits

---
+++ src/main/scala/k2b6s9j/BoatCraft/boat/Materials.scala
@@ -0,0 +1,29 @@
+package k2b6s9j.BoatCraft.boat
+
+object Materials {
+
+  trait Wood {
+
+  }
+
+  object Wood {
+
+    trait Oak extends Wood{
+
+    }
+
+    trait Spruce extends Wood{
+
+    }
+
+    trait Birch extends Wood {
+
+    }
+
+    trait Jungle extends Wood{
+
+    }
+
+  }
+
+}
