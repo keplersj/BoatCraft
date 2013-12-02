d6bf83b0db022b852e41643a6aba088742b0b2cf
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-01 19:01:13 -0700

    Chest modifiers!

--- src/main/scala/k2b6s9j/BoatCraft/boat/Modifiers.scala
+++ src/main/scala/k2b6s9j/BoatCraft/boat/Modifiers.scala
@@ -1,22 +1,58 @@
 package k2b6s9j.BoatCraft.boat
 
+import k2b6s9j.BoatCraft.boat.Boat.{EntityBoatContainer, EntityCustomBoat}
+import net.minecraft.item.ItemStack
+import net.minecraft.block.Block
+
 object Modifiers {
 
   object Entity {
 
-    trait Chest {
+    trait Block extends EntityCustomBoat {
+
+      override def doesBoatContainBlock(): Boolean = {
+        true
+      }
 
     }
 
-    trait Furnace {
+    trait Chest extends EntityBoatContainer with Block {
+
+      override def blockInBoat(): ItemStack = {
+        new ItemStack(Block.chest, 1, 0)
+      }
+
+      override def getSizeInventory(): Int = {
+        27
+      }
+
+      override def getDefaultDisplayTile(): net.minecraft.block.Block = {
+        Block.chest
+      }
+
+      override def getDefaultDisplayTileOffset(): Int = {
+        8
+      }
+
+      override def isInvNameLocalized(): Int = {
+        false
+      }
+
+      override def getInvName(): String = {
+        "Boat"
+      }
 
     }
 
-    trait TNT {
+    trait Furnace extends EntityCustomBoat with Block {
 
     }
 
-    trait Hopper {
+    trait TNT extends EntityCustomBoat with Block {
+
+    }
+
+    trait Hopper extends EntityBoatContainer with Block {
 
     }
 
