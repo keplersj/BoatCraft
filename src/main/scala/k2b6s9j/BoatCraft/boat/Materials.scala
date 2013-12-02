ae62f91bcc012b8b5681a37852e99686f86b6e03
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-01 18:39:14 -0700

    Finished basic material traits.

--- src/main/scala/k2b6s9j/BoatCraft/boat/Materials.scala
+++ src/main/scala/k2b6s9j/BoatCraft/boat/Materials.scala
@@ -1,26 +1,61 @@
 package k2b6s9j.BoatCraft.boat
 
 import k2b6s9j.BoatCraft.boat.Boat.{EntityCustomBoat, RenderBoat}
+import net.minecraft.world.World
+import net.minecraft.item.ItemStack
+import net.minecraft.block.Block
 
 object Materials {
 
   object Entity {
 
+    trait Entity extends EntityCustomBoat {
+
+      def Entity(world: World){
+        super.world
+      }
+
+      def Entity(world: World, x: Double, y: Double, z: Double) {
+        super.Entity(world, x, y, z)
+      }
+
+      override def isCustomBoat(): Boolean = {
+        true
+      }
+
+    }
+
     object Wood {
 
-      trait Oak extends EntityCustomBoat {
+      trait Oak extends EntityCustomBoat with Entity {
+
+        override def customPlank(): ItemStack = {
+          new ItemStack(Block.plank, 1, 0)
+        }
 
       }
 
-      trait Spruce extends EntityCustomBoat {
+      trait Spruce extends EntityCustomBoat with Entity {
+
+        override def customPlank(): ItemStack = {
+          new ItemStack(Block.plank, 1, 1)
+        }
 
       }
 
-      trait Birch extends EntityCustomBoat {
+      trait Birch extends EntityCustomBoat with Entity {
+
+        override def customPlank(): ItemStack = {
+          new ItemStack(Block.plank, 1, 2)
+        }
 
       }
 
-      trait Jungle extends EntityCustomBoat {
+      trait Jungle extends EntityCustomBoat with Entity {
+
+        override def customPlank(): ItemStack = {
+          new ItemStack(Block.plank, 1, 3)
+        }
 
       }
 
