de2b82debc335ae03522a7f5f7cdb895a31d19a4
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-01 19:05:12 -0700

    Furnace modifier!

--- src/main/scala/k2b6s9j/BoatCraft/boat/Modifiers.scala
+++ src/main/scala/k2b6s9j/BoatCraft/boat/Modifiers.scala
@@ -3,6 +3,7 @@ package k2b6s9j.BoatCraft.boat
 import k2b6s9j.BoatCraft.boat.Boat.{EntityBoatContainer, EntityCustomBoat}
 import net.minecraft.item.ItemStack
 import net.minecraft.block.Block
+import net.minecraft.entity.player.EntityPlayer
 
 object Modifiers {
 
@@ -12,6 +13,11 @@ object Modifiers {
 
       override def doesBoatContainBlock(): Boolean = {
         true
+      }
+
+      override def func_130002_c(player: EntityPlayer): Boolean = {
+        //Do not mount this boat! It contains a block!
+        false
       }
 
     }
@@ -45,6 +51,18 @@ object Modifiers {
     }
 
     trait Furnace extends EntityCustomBoat with Block {
+
+      override def blockInBoat(): ItemStack = {
+        new ItemStack(Block.furnaceIdle, 1, 0)
+      }
+
+      override def getDefaultDisplayTile(): net.minecraft.block.Block = {
+        Block.furnaceBurning
+      }
+
+      override def getDefaultDisplayTileOffset(): Int = {
+        2
+      }
 
     }
 
