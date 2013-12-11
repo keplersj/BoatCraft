5a4c6084d257788619cdf5144d29d52b373c38c8
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-11 14:32:33 -0700

    Have a dependency on Metrics now.
    Instead of Metrics Lite, matches cloaked batman.

--- src/main/scala/k2b6s9j/BoatCraft/registry/StatisticsRegistration.scala
+++ src/main/scala/k2b6s9j/BoatCraft/registry/StatisticsRegistration.scala
@@ -1,16 +1,14 @@
 package k2b6s9j.BoatCraft.registry
 
-import org.mcstats.MetricsLite
-import k2b6s9j.BoatCraft.BoatCraft
+import org.mcstats.Metrics
 import k2b6s9j.BoatCraft.utilities.log.ModLogger
 
 object StatisticsRegistration {
 
-  def mod:BoatCraft = mod
 
     def RegisterMCStats() {
       try {
-        var metrics: MetricsLite = new MetricsLite("BoatCraft", "2.0")
+        var metrics: Metrics = new Metrics("BoatCraft", "2.0")
         metrics.start()
       }
       catch
