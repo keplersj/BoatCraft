6a53b91dcbee460bfd6b4f16f07fac0b5ac32e40
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-19 22:52:45 -0700

    Testing the base texture in all of the oak wood boats

--- src/test/scala/k2b6s9j/BoatCraft/test/boat/wood/oak/ChestTest.scala
+++ src/test/scala/k2b6s9j/BoatCraft/test/boat/wood/oak/ChestTest.scala
@@ -3,15 +3,15 @@ package k2b6s9j.BoatCraft.test.boat.wood.oak
 import org.scalatest._
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import k2b6s9j.BoatCraft.boat.wood.oak.Empty
+import k2b6s9j.BoatCraft.boat.wood.oak.Chest
 import net.minecraft.util.ResourceLocation
 
 @RunWith(classOf[JUnitRunner])
-class EmptyTest extends FlatSpec with Matchers {
+class ChestTest extends FlatSpec with Matchers {
 
-  def renderer: Empty.Render = new Empty.Render
+  def renderer: Chest.Render = new Chest.Render
 
-  "The Empty Oak Wood Boat Renderer" should "return the Oak Wood Boat Texture." in {
+  "The Chest Oak Wood Boat Renderer" should "return the Oak Wood Boat Texture." in {
     renderer.getTexture() should be (new ResourceLocation("textures/entity/boat.png"))
   }
 
--- src/test/scala/k2b6s9j/BoatCraft/test/boat/wood/oak/FurnaceTest.scala
+++ src/test/scala/k2b6s9j/BoatCraft/test/boat/wood/oak/FurnaceTest.scala
@@ -3,15 +3,15 @@ package k2b6s9j.BoatCraft.test.boat.wood.oak
 import org.scalatest._
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import k2b6s9j.BoatCraft.boat.wood.oak.Empty
+import k2b6s9j.BoatCraft.boat.wood.oak.Furnace
 import net.minecraft.util.ResourceLocation
 
 @RunWith(classOf[JUnitRunner])
-class EmptyTest extends FlatSpec with Matchers {
+class FurnaceTest extends FlatSpec with Matchers {
 
-  def renderer: Empty.Render = new Empty.Render
+  def renderer: Furnace.Render = new Furnace.Render
 
-  "The Empty Oak Wood Boat Renderer" should "return the Oak Wood Boat Texture." in {
+  "The Furnace Oak Wood Boat Renderer" should "return the Oak Wood Boat Texture." in {
     renderer.getTexture() should be (new ResourceLocation("textures/entity/boat.png"))
   }
 
