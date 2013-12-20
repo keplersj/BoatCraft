493341d824f0e9f4432744c67eaa0ef67b62d97d
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-19 22:47:33 -0700

    Testing for Empty Oak Wood Boat Textures

---
+++ src/test/scala/k2b6s9j/BoatCraft/test/boat/wood/oak/EmptyTest.scala
@@ -0,0 +1,18 @@
+package k2b6s9j.BoatCraft.test.boat.wood.oak
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import k2b6s9j.BoatCraft.boat.wood.oak.Empty
+import net.minecraft.util.ResourceLocation
+
+@RunWith(classOf[JUnitRunner])
+class EmptyTest extends FlatSpec with Matchers {
+
+  def renderer: Empty.Render = new Empty.Render
+
+  "The Empty Oak Wood Boat Renderer" should "return the Empty Oak Wood Boat Texture." in {
+    renderer.getTexture() should be (new ResourceLocation("textures/entity/boat.png"))
+  }
+
+}
