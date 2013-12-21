614a06bc40733c1db9c17ed181b71f9ae4296a87
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-20 22:20:22 -0700

    Initial Rewrite of the the Remove Recipe method.

--- src/main/scala/k2b6s9j/BoatCraft/registry/RecipeRegistration.scala
+++ src/main/scala/k2b6s9j/BoatCraft/registry/RecipeRegistration.scala
@@ -6,8 +6,7 @@ import k2b6s9j.BoatCraft.utilities.log.ModLogger
 import net.minecraft.block.Block
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
-import net.minecraft.item.crafting.CraftingManager
-import net.minecraft.item.crafting.IRecipe
+import net.minecraft.item.crafting.{ShapelessRecipes, ShapedRecipes, CraftingManager, IRecipe}
 import net.minecraftforge.oredict.ShapedOreRecipe
 import net.minecraftforge.oredict.ShapelessOreRecipe
 
@@ -20,8 +19,20 @@ object RecipeRegistration {
     val recipes = CraftingManager.getInstance.getRecipeList.asInstanceOf[util.ArrayList[Any]]
     for (scan <- 0 until recipes.size) {
       val tmpRecipe = recipes.get(scan).asInstanceOf[IRecipe]
-      recipeResult = tmpRecipe.getRecipeOutput
-      if (ItemStack.areItemStacksEqual(resultItem, recipeResult)) {
+      if (tmpRecipe.isInstanceOf[ShapedRecipes])
+      {
+        val recipe: ShapedRecipes = tmpRecipe.asInstanceOf[ShapedRecipes]
+        recipeResult = recipe.getRecipeOutput
+      }
+
+      if (tmpRecipe.isInstanceOf[ShapelessRecipes])
+      {
+        val recipe: ShapelessRecipes = tmpRecipe.asInstanceOf[ShapelessRecipes]
+        recipeResult = recipe.getRecipeOutput
+      }
+
+      if (ItemStack.areItemStacksEqual(resultItem, recipeResult))
+      {
         ModLogger.info("Removed Recipe: " + recipes.get(scan) + " -> " + recipeResult)
         recipes.remove(scan)
       }
