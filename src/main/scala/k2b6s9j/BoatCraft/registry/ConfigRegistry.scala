163252aefb4b284b7b9ca1a03431394d782ffbce
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-21 14:20:10 -0700

    Redid the Vanilla Boat Item IDs Configuration Organization

--- src/main/scala/k2b6s9j/BoatCraft/registry/ConfigRegistry.scala
+++ src/main/scala/k2b6s9j/BoatCraft/registry/ConfigRegistry.scala
@@ -14,34 +14,52 @@ object ConfigRegistry {
     val config = new Configuration(file)
     config.load()
 
-    LoadItems(config)
+    LoadVanillaBoats(config)
 
     if ( config.hasChanged() ) {
       config.save()
     }
   }
 
-  private def LoadItems (cfg: Configuration): Unit = {
-    //Boats
-    oak.Empty.Item.ID = cfg.getItem(itemBoats, "Oak Boat", 25500).getInt
-    oak.Chest.Item.ID = cfg.getItem(itemBoats, "Oak Chest Boat", 25501).getInt
-    oak.Furnace.Item.ID = cfg.getItem(itemBoats, "Oak Furnace Boat", 25502).getInt
-    oak.Hopper.Item.ID = cfg.getItem(itemBoats, "Oak Hopper Boat", 25503).getInt
-    oak.TNT.Item.ID = cfg.getItem(itemBoats, "Oak TNT Boat", 25504).getInt
-    spruce.Empty.Item.ID = cfg.getItem(itemBoats, "Spruce Boat", 25505).getInt
-    spruce.Chest.Item.ID = cfg.getItem(itemBoats, "Spruce Chest Boat", 25506).getInt
-    spruce.Furnace.Item.ID = cfg.getItem(itemBoats, "Spruce Furnace Boat", 25507).getInt
-    spruce.Hopper.Item.ID = cfg.getItem(itemBoats, "Spruce Hopper Boat", 25508).getInt
-    spruce.TNT.Item.ID = cfg.getItem(itemBoats, "Spruce TNT Boat", 25509).getInt
-    birch.Empty.Item.ID = cfg.getItem(itemBoats, "Birch Boat", 25510).getInt
-    birch.Chest.Item.ID = cfg.getItem(itemBoats, "Birch Chest Boat", 25511).getInt
-    birch.Furnace.Item.ID = cfg.getItem(itemBoats, "Birch Furnace Boat", 25512).getInt
-    birch.Hopper.Item.ID = cfg.getItem(itemBoats, "Birch Hopper Boat", 25513).getInt
-    birch.TNT.Item.ID = cfg.getItem(itemBoats, "Birch TNT Boat", 25514).getInt
-    jungle.Empty.Item.ID = cfg.getItem(itemBoats, "Jungle Boat", 25515).getInt
-    jungle.Chest.Item.ID = cfg.getItem(itemBoats, "Jungle Chest Boat", 25516).getInt
-    jungle.Furnace.Item.ID = cfg.getItem(itemBoats, "Jungle Furnace Boat", 25517).getInt
-    jungle.Hopper.Item.ID = cfg.getItem(itemBoats, "Jungle Hopper Boat", 25518).getInt
-    jungle.TNT.Item.ID = cfg.getItem(itemBoats, "Jungle TNT Boat", 25519).getInt
+  private def LoadVanillaBoats (cfg: Configuration): Unit = {
+    /*
+    Loading the IDs for Vanilla Wood Boats
+     */
+
+    /*
+    Oak Wood Boats
+     */
+    oak.Empty.Item.ID = cfg.getItem(itemBoats, "Empty Oak Wood Boat", 25500).getInt
+    oak.Chest.Item.ID = cfg.getItem(itemBoats, "Chest Oak Wood Boat", 25501).getInt
+    oak.Furnace.Item.ID = cfg.getItem(itemBoats, "Furnace Oak Wood Boat", 25502).getInt
+    oak.Hopper.Item.ID = cfg.getItem(itemBoats, "Hopper Oak Wood Boat", 25503).getInt
+    oak.TNT.Item.ID = cfg.getItem(itemBoats, "TNT Oak Wood Boat", 25504).getInt
+
+    /*
+    Spruce Wood Boats
+     */
+    spruce.Empty.Item.ID = cfg.getItem(itemBoats, "Empty Spruce Wood Boat", 25505).getInt
+    spruce.Chest.Item.ID = cfg.getItem(itemBoats, "Chest Spruce Wood Boat", 25506).getInt
+    spruce.Furnace.Item.ID = cfg.getItem(itemBoats, "Furnace Spruce Wood Boat", 25507).getInt
+    spruce.Hopper.Item.ID = cfg.getItem(itemBoats, "Hopper Spruce Wood Boat", 25508).getInt
+    spruce.TNT.Item.ID = cfg.getItem(itemBoats, "TNT Spruce Wood Boat Boat", 25509).getInt
+
+    /*
+    Birch Wood Boats
+     */
+    birch.Empty.Item.ID = cfg.getItem(itemBoats, "Empty Birch Wood Boat", 25510).getInt
+    birch.Chest.Item.ID = cfg.getItem(itemBoats, "Chest Birch Wood Boat", 25511).getInt
+    birch.Furnace.Item.ID = cfg.getItem(itemBoats, "Furnace Birch Wood Boat", 25512).getInt
+    birch.Hopper.Item.ID = cfg.getItem(itemBoats, "Hopper Birch Wood Boat", 25513).getInt
+    birch.TNT.Item.ID = cfg.getItem(itemBoats, "TNT Birch Wood Boat", 25514).getInt
+
+    /*
+    Jungle Wood Boats
+     */
+    jungle.Empty.Item.ID = cfg.getItem(itemBoats, "Empty Jungle Wood Boat", 25515).getInt
+    jungle.Chest.Item.ID = cfg.getItem(itemBoats, "Chest Jungle Wood Boat", 25516).getInt
+    jungle.Furnace.Item.ID = cfg.getItem(itemBoats, "Furnace Jungle Wood Boat", 25517).getInt
+    jungle.Hopper.Item.ID = cfg.getItem(itemBoats, "Hopper Jungle Wood Boat", 25518).getInt
+    jungle.TNT.Item.ID = cfg.getItem(itemBoats, "TNT Jungle Wood Boat", 25519).getInt
   }
 }
