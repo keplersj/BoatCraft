722cee903d6853fcfa9fe438ad81c02c49e47099
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-20 17:48:55 -0700

    Nevermind…

--- src/test/scala/k2b6s9j/BoatCraft/test/boat/MaterialsTest.scala
+++ src/test/scala/k2b6s9j/BoatCraft/test/boat/MaterialsTest.scala
@@ -3,16 +3,8 @@ package k2b6s9j.BoatCraft.test.boat
 import org.scalatest._
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import k2b6s9j.BoatCraft.boat.Materials
-import k2b6s9j.BoatCraft.boat.Materials.Entity.Entity
 
 @RunWith(classOf[JUnitRunner])
 class MaterialsTest extends FlatSpec with Matchers {
-
-  def entity: Materials.Entity.Entity = new Entity {}
-
-  "Any boat interacting with this class" should "be a custom boat." in {
-    entity.isCustomBoat() should be (true)
-  }
 
 }
