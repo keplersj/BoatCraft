824e3611d30ac4862f17e031b30d93091d789d91
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-21 16:41:14 -0700

    Skeleton Empty Boat Classes for the Thaumic Boats.

---
+++ src/main/scala/k2b6s9j/BoatCraft/compatibility/thaumcraft/boat/wood/greatwood/Empty.scala
@@ -0,0 +1,29 @@
+package k2b6s9j.BoatCraft.compatibility.thaumcraft.boat.wood.greatwood
+
+import net.minecraft.world.World
+import k2b6s9j.BoatCraft.boat.Boat.{RenderBoat, ItemCustomBoat, EntityCustomBoat}
+import k2b6s9j.BoatCraft.compatibility.thaumcraft.boat.Materials
+
+object Empty {
+
+  class Entity(world: World) extends EntityCustomBoat(world) with Materials.Entity.Wood.Greatwood {
+
+  }
+
+  object Item {
+
+    var ID: Int = _
+    var shiftedID: Int = _
+    var item: Item = new Item(ID)
+
+  }
+
+  class Item(id: Int) extends ItemCustomBoat(id) {
+
+  }
+
+  class Render extends RenderBoat with Materials.Render.Wood.Greatwood {
+
+  }
+
+}
---
+++ src/main/scala/k2b6s9j/BoatCraft/compatibility/thaumcraft/boat/wood/silverwood/Empty.scala
@@ -0,0 +1,29 @@
+package k2b6s9j.BoatCraft.compatibility.thaumcraft.boat.wood.silverwood
+
+import k2b6s9j.BoatCraft.boat.Boat.{RenderBoat, ItemCustomBoat, EntityCustomBoat}
+import k2b6s9j.BoatCraft.compatibility.thaumcraft.boat.Materials
+import net.minecraft.world.World
+
+object Empty {
+
+  class Entity(world: World) extends EntityCustomBoat(world) with Materials.Entity.Wood.Silverwood {
+
+  }
+
+  object Item {
+
+    var ID: Int = _
+    var shiftedID: Int = _
+    var item: Item = new Item(ID)
+
+  }
+
+  class Item(id: Int) extends ItemCustomBoat(id) {
+
+  }
+
+  class Render extends RenderBoat with Materials.Render.Wood.Silverwood {
+
+  }
+
+}
