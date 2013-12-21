e42f6a58e9c59c1fe61a865b97e50026153ce5dc
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-20 21:59:56 -0700

    Fix TNT Birch Wood Boat Registration

--- src/main/scala/k2b6s9j/BoatCraft/boat/wood/birch/TNT.scala
+++ src/main/scala/k2b6s9j/BoatCraft/boat/wood/birch/TNT.scala
@@ -33,7 +33,7 @@ object TNT {
 
     setUnlocalizedName("boat.wood.birch.furnace")
     //func_111206_d("boatcraft:boat.wood.birch.furnace")
-    GameRegistry.registerItem(this, "Furnace Birch Wood Boat")
+    GameRegistry.registerItem(this, "TNT Birch Wood Boat")
     Item.shiftedID = this.itemID
     OreDictionary.registerOre("boatBirchWoodFurnace", new ItemStack(this))
 
