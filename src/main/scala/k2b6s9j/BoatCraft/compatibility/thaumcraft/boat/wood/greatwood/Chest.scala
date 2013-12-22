027b99f46a1108eacb0b7a537af44c27c453dd9c
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-21 18:06:35 -0700

    Thaumic Chest Boats!

--- src/main/scala/k2b6s9j/BoatCraft/compatibility/thaumcraft/boat/wood/greatwood/Chest.scala
+++ src/main/scala/k2b6s9j/BoatCraft/compatibility/thaumcraft/boat/wood/greatwood/Chest.scala
@@ -1,5 +1,43 @@
 package k2b6s9j.BoatCraft.compatibility.thaumcraft.boat.wood.greatwood
 
+import net.minecraft.world.World
+import k2b6s9j.BoatCraft.boat.Boat.{RenderBoat, ItemCustomBoat, EntityCustomBoat}
+import k2b6s9j.BoatCraft.compatibility.thaumcraft.boat.Materials
+import cpw.mods.fml.common.registry.GameRegistry
+import k2b6s9j.BoatCraft.boat.Modifiers
+
 object Chest {
+
+  class Entity(world: World) extends EntityCustomBoat(world) with Materials.Entity.Wood.Greatwood with Modifiers.Entity.Chest {
+
+    override def useItemID(): Boolean = {
+      true
+    }
+
+    override def customBoatItemID(): Int = {
+      Item.shiftedID
+    }
+
+  }
+
+  object Item {
+
+    var ID: Int = _
+    var shiftedID: Int = _
+    var item: Item = new Item(ID)
+
+  }
+
+  class Item(id: Int) extends ItemCustomBoat(id) {
+
+    setUnlocalizedName("boat.compatibility.wood.greatwood.chest")
+    GameRegistry.registerItem(this, "Chest Greatwood Boat")
+    Item.shiftedID = this.itemID
+
+  }
+
+  class Render extends RenderBoat with Materials.Render.Wood.Greatwood with Modifiers.Render.Chest {
+
+  }
 
 }
