22bddafd70bab65918c066ecc0d62aabca58084b
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-11 14:47:43 -0700

    Starting fresh works too.

--- core/src/main/scala/k2b6s9j/BoatCraft/core/Boat.scala
+++ core/src/main/scala/k2b6s9j/BoatCraft/core/Boat.scala
@@ -23,139 +23,41 @@ object Boat {
 
   case class EntityCustomBoat(world: World, x: Double, y: Double, z: Double) extends EntityBoat(world, x, y, z)
 
-  abstract class EntityBoatContainer(world: World, x: Double, y: Double, z: Double) extends EntityCustomBoat(world, x, y, z) with IInventory {
+  case class EntityBoatContainer(world2: World, x2: Double, y2: Double, z2: Double) extends EntityCustomBoat(world2, x2, y2, z2) with IInventory{
 
-    private var boatContainerItems: Array[ItemStack] = new Array[ItemStack](36)
+    def getSizeInventory: Int = null
 
-    private var dropContentsWhenDead: Boolean = true
+    def getStackInSlot(p1: Int): ItemStack = null
 
-    def getStackInSlot(par1: Int): ItemStack = this.boatContainerItems(par1)
+    def decrStackSize(p1: Int, p2: Int): ItemStack = null
 
-    def decrStackSize(par1: Int, par2: Int): ItemStack = {
-      if (this.boatContainerItems(par1) != null) {
-        var itemstack: ItemStack = null
-        if (this.boatContainerItems(par1).stackSize <= par2) {
-          itemstack = this.boatContainerItems(par1)
-          this.boatContainerItems(par1) = null
-          itemstack
-        } else {
-          itemstack = this.boatContainerItems(par1).splitStack(par2)
-          if (this.boatContainerItems(par1).stackSize == 0) {
-            this.boatContainerItems(par1) = null
-          }
-          itemstack
-        }
-      } else {
-        null
-      }
+    def getStackInSlotOnClosing(p1: Int): ItemStack = null
+
+    def setInventorySlotContents(p1: Int, p2: ItemStack): Unit = {
+
     }
 
-    def getStackInSlotOnClosing(par1: Int): ItemStack = {
-      if (this.boatContainerItems(par1) != null) {
-        val itemstack = this.boatContainerItems(par1)
-        this.boatContainerItems(par1) = null
-        itemstack
-      } else {
-        null
-      }
+    def func_145825_b(): String = "something"
+
+    def func_145818_k_(): Boolean = ture
+
+    def getInventoryStackLimit: Int = 0
+
+    def onInventoryChanged(): Unit = {
+
     }
 
-    def setInventorySlotContents(par1: Int, par2ItemStack: ItemStack) {
-      this.boatContainerItems(par1) = par2ItemStack
-      if (par2ItemStack != null &&
-        par2ItemStack.stackSize > this.getInventoryStackLimit) {
-        par2ItemStack.stackSize = this.getInventoryStackLimit
-      }
+    def isUseableByPlayer(p1: EntityPlayer): Boolean = true
+
+    def openChest(): Unit = {
+
     }
 
-    def onInventoryChanged() {
+    def closeChest(): Unit = {
+
     }
 
-    def isUseableByPlayer(par1EntityPlayer: EntityPlayer): Boolean = {
-      if (this.isDead) false else par1EntityPlayer.getDistanceSqToEntity(this) <= 64.0D
-    }
-
-    def openChest() {
-    }
-
-    def closeChest() {
-    }
-
-    def isItemValidForSlot(par1: Int, par2ItemStack: ItemStack): Boolean = true
-
-    def getInvName(): String = "container.boat"
-
-    def getInventoryStackLimit(): Int = 64
-
-    override def travelToDimension(par1: Int) {
-      this.dropContentsWhenDead = false
-      super.travelToDimension(par1)
-    }
-
-    override protected def writeEntityToNBT(par1NBTTagCompound: NBTTagCompound) {
-      super.writeEntityToNBT(par1NBTTagCompound)
-      val nbttaglist = new NBTTagList()
-      for (i <- 0 until this.boatContainerItems.length if this.boatContainerItems(i) != null) {
-        val nbttagcompound1 = new NBTTagCompound()
-        nbttagcompound1.setByte("Slot", i.toByte)
-        this.boatContainerItems(i).writeToNBT(nbttagcompound1)
-        nbttaglist.appendTag(nbttagcompound1)
-      }
-      par1NBTTagCompound.setTag("Items", nbttaglist)
-    }
-
-    override def interactFirst(player: EntityPlayer): Boolean = {
-      if (player.isSneaking) {
-        return false
-      }
-      if (!this.worldObj.isRemote) {
-        player.displayGUIChest(this)
-      }
-      true
-    }
-
-    def dropContents() {
-      for (i <- 0 until this.getSizeInventory) {
-        val itemstack = this.getStackInSlot(i)
-        if (itemstack != null) {
-          val f = this.rand.nextFloat() * 0.8F + 0.1F
-          val f1 = this.rand.nextFloat() * 0.8F + 0.1F
-          val f2 = this.rand.nextFloat() * 0.8F + 0.1F
-          while (itemstack.stackSize > 0) {
-            var j = this.rand.nextInt(21) + 10
-            if (j > itemstack.stackSize) {
-              j = itemstack.stackSize
-            }
-            itemstack.stackSize -= j
-            val entityitem = new EntityItem(this.worldObj, this.posX + f.toDouble, this.posY + f1.toDouble,
-              this.posZ + f2.toDouble, new ItemStack(itemstack, j, itemstack.getItemDamage))
-            val f3 = 0.05F
-            entityitem.motionX = (this.rand.nextGaussian().toFloat * f3).toDouble
-            entityitem.motionY = (this.rand.nextGaussian().toFloat * f3 + 0.2F).toDouble
-            entityitem.motionZ = (this.rand.nextGaussian().toFloat * f3).toDouble
-            this.worldObj.spawnEntityInWorld(entityitem)
-          }
-        }
-      }
-    }
-
-    override def crashedDrops() {
-      dropContents()
-      var k: Int = 0
-      k = 0
-      while (k < 3) {
-        this.dropItemWithOffset(Block.planks.blockID, 1, 0.0F)
-        k
-      }
-      k = 0
-      while (k < 2) {
-        this.dropItemWithOffset(Item.stick.itemID, 1, 0.0F)
-        k
-      }
-      if (doesBoatContainBlock()) {
-        this.entityDropItem(blockInBoat(), 0.0F)
-      }
-    }
+    def isItemValidForSlot(p1: Int, p2: ItemStack): Boolean = true
   }
 
   class RenderCustomBoat extends RenderBoat with IItemRenderer {
