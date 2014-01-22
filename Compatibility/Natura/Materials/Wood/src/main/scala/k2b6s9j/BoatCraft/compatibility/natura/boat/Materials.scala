579f341d91d9f17986905b4f75fc110c8cc07a64
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-21 19:56:25 -0700

    Natura Wood.

---
+++ Compatibility/Natura/Materials/Wood/src/main/scala/k2b6s9j/BoatCraft/compatibility/natura/boat/Materials.scala
@@ -0,0 +1,37 @@
+package k2b6s9j.BoatCraft.compatibility.natura.boat
+
+object Materials {
+
+  object Wood {
+
+    trait Maple {
+
+    }
+
+    trait Silverbell {
+
+    }
+
+    trait Amaranth {
+
+    }
+
+    trait Tiger {
+
+    }
+
+    trait Willow {
+
+    }
+
+    trait Darkwood {
+
+    }
+
+    trait Fusewood {
+
+    }
+
+  }
+
+}
