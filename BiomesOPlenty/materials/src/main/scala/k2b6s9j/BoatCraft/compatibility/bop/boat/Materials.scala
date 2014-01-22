1659b5374ca1998edf578811756348c8ed3d2638
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-21 18:02:11 -0700

    Biomes O Plenty Wooden Materials.

---
+++ BiomesOPlenty/materials/src/main/scala/k2b6s9j/BoatCraft/compatibility/bop/boat/Materials.scala
@@ -0,0 +1,69 @@
+package k2b6s9j.BoatCraft.compatibility.bop.boat
+
+object Materials {
+
+  object Wood {
+
+    trait ScaredOak {
+
+    }
+
+    trait Cherry {
+
+    }
+
+    trait Dark {
+
+    }
+
+    trait Fir {
+
+    }
+
+    trait Holy {
+
+    }
+
+    trait Loftwood {
+
+    }
+
+    trait Magic {
+
+    }
+
+    trait Mangrove {
+
+    }
+
+    trait Palm {
+
+    }
+
+    trait Redwood {
+
+    }
+
+    trait Willow {
+
+    }
+
+    trait Bambo {
+
+    }
+
+    trait Pine {
+
+    }
+
+    trait Hellbark {
+
+    }
+
+    trait Jacaranda {
+
+    }
+    
+  }
+
+}
