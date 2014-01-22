ac5eaed34ac3b8394d23193bdf1fc5dffadaf2bd
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-21 18:45:06 -0700

    Vanilla Wooden Materials

---
+++ vanilla/materials/src/main/scala/k2b6s9j/BoatCraft/compatibility/vanilla/VanillaMaterials.scala
@@ -0,0 +1,8 @@
+package k2b6s9j.BoatCraft.compatibility.vanilla
+
+import cpw.mods.fml.common.Mod
+
+@Mod(name = "BoatCraft: Compatibility: Vanilla: Materials: Wood", modid = "boatcraft:compatibility:vanilla:materials:wood", modLanguage = "scala", dependencies = "required-after:BoatCraft@2.0")
+object VanillaMaterials {
+
+}
---
+++ vanilla/materials/src/main/scala/k2b6s9j/BoatCraft/compatibility/vanilla/boat/Materials.scala
@@ -0,0 +1,33 @@
+package k2b6s9j.BoatCraft.compatibility.vanilla.boat
+
+object Materials {
+
+  object Wood {
+
+    trait Oak {
+
+    }
+
+    trait Spruce {
+
+    }
+
+    trait Birch {
+
+    }
+
+    trait Jungle {
+
+    }
+
+    trait Acacia {
+
+    }
+
+    trait DarkOak {
+
+    }
+
+  }
+
+}
