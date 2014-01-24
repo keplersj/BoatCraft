d7f3490e7c624d14120f1f754ed3d05554b8f4e1
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-24 00:21:01 -0700

    Central Boat Registry

--- Core/src/main/scala/k2b6s9j/BoatCraft/core/Boat.scala
+++ Core/src/main/scala/k2b6s9j/BoatCraft/core/Boat.scala
@@ -13,7 +13,7 @@ import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.ItemStack
 import net.minecraft.world.World
 
-object Boat {
+class Boat {
 
   class ItemCustomBoat extends ItemBoat
 
---
+++ Core/src/main/scala/k2b6s9j/BoatCraft/core/registry/BoatRegistry.scala
@@ -0,0 +1,9 @@
+package k2b6s9j.BoatCraft.core.registry
+
+import k2b6s9j.BoatCraft.core.Boat
+
+object BoatRegistry {
+
+  var boats: Array[Boat] = new Array[Boat](0)
+
+}
