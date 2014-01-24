1631f42381cce328950139e1d2b68e499223813b
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-23 21:27:41 -0700

    Methods to add Materials to the Boat Material Matrix.
    To accomplish this Materials must be subclasses of the new Material trait.

--- Core/src/main/scala/k2b6s9j/BoatCraft/core/registry/MaterialRegistry.scala
+++ Core/src/main/scala/k2b6s9j/BoatCraft/core/registry/MaterialRegistry.scala
@@ -1,5 +1,17 @@
 package k2b6s9j.BoatCraft.core.registry
 
+import k2b6s9j.BoatCraft.core.traits.Material
+
 object MaterialRegistry {
+
+  var materials: Array[Material] = new Array[Material](0)
+
+  def addMaterial(newMaterial: Material) {
+    newMaterial +: materials
+  }
+
+  def addMaterials(newMaterials: Array[Material]) {
+    newMaterials ++: materials
+  }
 
 }
---
+++ Core/src/main/scala/k2b6s9j/BoatCraft/core/traits/Material.scala
@@ -0,0 +1,5 @@
+package k2b6s9j.BoatCraft.core.traits
+
+trait Material {
+
+}
