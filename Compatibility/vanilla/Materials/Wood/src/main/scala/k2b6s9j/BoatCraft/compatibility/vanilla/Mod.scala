7da29c5aeead9b09faabc72988ce15319d000330
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-24 21:56:13 -0700

    Load Before BoatCraft. This will enforce the matrix loads everything correctly.

--- Compatibility/vanilla/Materials/Wood/src/main/scala/k2b6s9j/BoatCraft/compatibility/vanilla/Mod.scala
+++ Compatibility/vanilla/Materials/Wood/src/main/scala/k2b6s9j/BoatCraft/compatibility/vanilla/Mod.scala
@@ -6,7 +6,7 @@ import cpw.mods.fml.common.Mod.EventHandler
 import k2b6s9j.BoatCraft.core.registry.MaterialRegistry
 import k2b6s9j.BoatCraft.compatibility.vanilla.boat.wood._
 
-@Mod(name = "BoatCraft:Compatibility:Vanilla:Materials:Wood", modid = "BoatCraft:Compatibility:Vanilla:Materials:Wood", modLanguage = "scala", dependencies = "required-after:BoatCraft")
+@Mod(name = "BoatCraft:Compatibility:Vanilla:Materials:Wood", modid = "BoatCraft:Compatibility:Vanilla:Materials:Wood", modLanguage = "scala", dependencies = "required-before:BoatCraft")
 object Mod {
 
   @EventHandler
