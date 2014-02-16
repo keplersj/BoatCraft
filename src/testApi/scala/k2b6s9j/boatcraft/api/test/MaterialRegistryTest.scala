19e78121dbd848f38327b34dd95639fe76d2915f
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-02-16 12:15:22 -0700

    First successful test for the API.
    Even though Gradle is double-testing.

---
+++ src/testApi/scala/k2b6s9j/boatcraft/api/test/MaterialRegistryTest.scala
@@ -0,0 +1,19 @@
+package k2b6s9j.boatcraft.api.test
+
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+
+import org.junit.runner.RunWith
+
+import k2b6s9j.boatcraft.api.registry.MaterialRegistry
+import k2b6s9j.boatcraft.api.traits.examples._
+
+@RunWith(classOf[JUnitRunner])
+class MaterialRegistryTest extends FlatSpec with Matchers with BeforeAndAfter {
+
+  "A Material" should "be added to the registered Materials map." in {
+    MaterialRegistry addMaterial ExampleMaterial
+    MaterialRegistry getMaterial "test" shouldBe ExampleMaterial
+  }
+
+}
