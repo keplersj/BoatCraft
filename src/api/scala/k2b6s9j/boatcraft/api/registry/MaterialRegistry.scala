a54b99ee49a66679d776e1df9fef2bd539c47a41
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-02-16 00:23:11 -0700

    Added comment for addMaterials()

--- src/api/scala/k2b6s9j/boatcraft/api/registry/MaterialRegistry.scala
+++ src/api/scala/k2b6s9j/boatcraft/api/registry/MaterialRegistry.scala
@@ -12,11 +12,19 @@ object MaterialRegistry
 {
 	var materials: Map[String, Material] = new HashMap[String, Material]
 	
+  /** Adds a single Material to the Map used by BoatCraft:Core for boat creation.
+    *
+    * @param newMaterial the Material being registered
+    */
 	def addMaterial(newMaterial: Material)
 	{
 		materials put(newMaterial toString, newMaterial)
 	}
 
+  /** Adds a list of Materials to the Map used by BoatCraft:Core for boat creation.
+    *
+    * @param newMaterials list of Materials being registered
+    */
 	def addMaterials(newMaterials: List[Material])
 	{
 		for (material <- newMaterials)
