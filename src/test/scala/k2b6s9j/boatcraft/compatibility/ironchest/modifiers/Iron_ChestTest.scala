a626f8385965b4ce995b820475dc181df49e9542
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-02-17 11:22:48 -0700

    Iron Chest Modifier Testing based on the Vanilla Chest Modifier Test.

--- src/test/scala/k2b6s9j/boatcraft/compatibility/ironchest/modifiers/Iron_ChestTest.scala
+++ src/test/scala/k2b6s9j/boatcraft/compatibility/ironchest/modifiers/Iron_ChestTest.scala
@@ -2,9 +2,43 @@ package k2b6s9j.boatcraft.compatibility.ironchest.modifiers
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{BeforeAndAfter, Matchers, FlatSpec}
+import org.scalatest._
+import k2b6s9j.boatcraft.api.traits.Modifier
+import cpw.mods.ironchest.{IronChestType, IronChest}
 
 @RunWith(classOf[JUnitRunner])
 class Iron_ChestTest extends FlatSpec with Matchers with BeforeAndAfter {
+
+  var modifier: Modifier = null
+
+  before {
+    modifier = Iron_Chest
+  }
+
+  "The Chest Modifier" should "be a modifier." in {
+    modifier shouldBe a [Modifier]
+  }
+
+  it should "not be rideable." in {
+    modifier.isRideable shouldBe false
+  }
+
+  it should "contain the Iron Chest block." in {
+    modifier.getBlock shouldBe IronChest.ironChestBlock
+  }
+
+  it should "have the metadata of an Iron Chest" in {
+    modifier.getMeta shouldBe IronChestType.IRON.ordinal
+  }
+
+  it should "be called \"Iron Chest\"." in {
+    modifier.getName shouldBe "Iron Chest"
+  }
+
+  /*
+  it should "contain a Chest." in {
+    modifier.getContent shouldBe new ItemStack(Blocks.chest)
+  }
+  */
 
 }
