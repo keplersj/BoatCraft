ac71dad118412d49a8fc3691b56041533b7a204d
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-02-18 20:06:53 -0700

    Testing for a boat items unlocalized name.

---
+++ src/test/scala/k2b6s9j/boatcraft/test/api/BoatTest.scala
@@ -0,0 +1,52 @@
+package k2b6s9j.boatcraft.test.api
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import net.minecraft.init.Items
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NBTTagCompound
+import k2b6s9j.boatcraft.api.Boat
+
+object BoatTest
+{
+
+  @RunWith(classOf[JUnitRunner])
+  class ItemCustomBoatTest extends FlatSpec with Matchers with BeforeAndAfter
+  {
+    var item: Boat.ItemCustomBoat = null
+
+    before
+    {
+      item = new Boat.ItemCustomBoat
+    }
+
+    "A Boat Item" should "have an unlocalized name based on it's Material and Modifier." in
+    {
+      val stack = new ItemStack(Items.chainmail_helmet)
+      stack.stackTagCompound = new NBTTagCompound
+      stack.stackTagCompound setString ("material", "test")
+      stack.stackTagCompound setString ("modifier", "test")
+      item.getUnlocalizedName(stack) shouldBe "boat.test.test"
+    }
+  }
+
+  @RunWith(classOf[JUnitRunner])
+  class EntityCustomBoatTest extends FlatSpec with Matchers with BeforeAndAfter
+  {
+
+  }
+
+  @RunWith(classOf[JUnitRunner])
+  class EntityBoatContainerTest extends FlatSpec with Matchers with BeforeAndAfter
+  {
+
+  }
+
+  @RunWith(classOf[JUnitRunner])
+  class RenderCustomBoatTest extends FlatSpec with Matchers with BeforeAndAfter
+  {
+
+  }
+
+}
