6af88fc7b1efd1e7c9e124421fa1b3a346041e00
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-02-18 19:16:58 -0700

    Basic testing for all metallic materials.

--- src/test/scala/k2b6s9j/boatcraft/test/compatibility/vanilla/materials/metal/GoldTest.scala
+++ src/test/scala/k2b6s9j/boatcraft/test/compatibility/vanilla/materials/metal/GoldTest.scala
@@ -1,10 +1,37 @@
-package k2b6s9j.boatcraft.compatibility.vanilla.materials.metal
+package k2b6s9j.boatcraft.test.compatibility.vanilla.materials.metal
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfter, Matchers, FlatSpec}
+import k2b6s9j.boatcraft.api.traits.Material
+import net.minecraft.util.ResourceLocation
+import k2b6s9j.boatcraft.compatibility.vanilla.materials.metal.Gold
 
 @RunWith(classOf[JUnitRunner])
 class GoldTest extends FlatSpec with Matchers with BeforeAndAfter {
+
+  var material: Material = null
+
+  before {
+    material = Gold
+  }
+
+  "The Gold Material" should "be a material." in {
+    material shouldBe a [Material]
+  }
+
+  it should "have a defined texture." in {
+    material.getTexture shouldBe new ResourceLocation("boatcraft", "textures/entity/boat/vanilla/metal/gold.png")
+  }
+
+  it should "be named \"Gold\"." in {
+    material.getName shouldBe "Gold"
+  }
+
+  /* TODO: Find a way to test ItemStack
+  it should "be made of Gold." in {
+    material.getItem shouldBe new ItemStack(Items.goldIngot)
+  }
+  */
 
 }
--- src/test/scala/k2b6s9j/boatcraft/test/compatibility/vanilla/materials/metal/IronTest.scala
+++ src/test/scala/k2b6s9j/boatcraft/test/compatibility/vanilla/materials/metal/IronTest.scala
@@ -1,10 +1,37 @@
-package k2b6s9j.boatcraft.compatibility.vanilla.materials.metal
+package k2b6s9j.boatcraft.test.compatibility.vanilla.materials.metal
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfter, Matchers, FlatSpec}
+import k2b6s9j.boatcraft.api.traits.Material
+import net.minecraft.util.ResourceLocation
+import k2b6s9j.boatcraft.compatibility.vanilla.materials.metal.Iron
 
 @RunWith(classOf[JUnitRunner])
 class IronTest extends FlatSpec with Matchers with BeforeAndAfter {
+
+  var material: Material = null
+
+  before {
+    material = Iron
+  }
+
+  "The Iron Material" should "be a material." in {
+    material shouldBe a [Material]
+  }
+
+  it should "have a defined texture." in {
+    material.getTexture shouldBe new ResourceLocation("boatcraft", "textures/entity/boat/vanilla/metal/iron.png")
+  }
+
+  it should "be named \"Iron\"." in {
+    material.getName shouldBe "Iron"
+  }
+
+  /* TODO: Find a way to test ItemStack
+  it should "be made of Emerald." in {
+    material.getItem shouldBe new ItemStack(Items.ironIngot)
+  }
+  */
 
 }
