68784dfb9626f2263aa8269444edd2bb703bb42c
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-02-28 22:50:44 -0700

    Deprecated Material Registry
    Pointed it's registration methods to the new Universal Registration method.

--- src/main/scala/k2b6s9j/boatcraft/api/registry/MaterialRegistry.scala
+++ src/main/scala/k2b6s9j/boatcraft/api/registry/MaterialRegistry.scala
@@ -7,30 +7,34 @@ import scala.collection.JavaConversions.asScalaBuffer
 import k2b6s9j.boatcraft.api.traits.Material
 import net.minecraft.item.ItemStack
 import net.minecraft.util.ResourceLocation
+import k2b6s9j.boatcraft.api.Registry
 
 /** Contains the methods needed to register Materials with BoatCraft:Core. */
+@deprecated
 object MaterialRegistry
 {
   /** The Map containing all of the registered Materials for BoatCraft:Core to create boats with. */
-  var materials: Map[String, Material] = new HashMap[String, Material]
+  @deprecated
+  val materials: Map[String, Material] = Registry.materials
 	
   /** Adds a single Material to the Map used by BoatCraft:Core for boat creation.
     *
     * @param newMaterial the Material being registered
     */
+  @deprecated
 	def addMaterial(newMaterial: Material)
 	{
-		materials put(newMaterial toString, newMaterial)
+		Registry register newMaterial
 	}
 
   /** Adds a list of Materials to the Map used by BoatCraft:Core for boat creation.
     *
     * @param newMaterials list of Materials being registered
     */
-	def addMaterials(newMaterials: List[Material])
+	@deprecated
+  def addMaterials(newMaterials: List[Material])
 	{
-		for (material <- newMaterials)
-			materials put(material toString, material)
+		Registry register newMaterials
 	}
 
   /** Returns a registered Material associated with a certain name.
