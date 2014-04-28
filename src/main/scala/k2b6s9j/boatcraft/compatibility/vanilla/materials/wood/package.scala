de6df0266fccd807914c88b79fc59ab3ee0dd1d0
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-04-27 18:20:25 -0600

    Fix Wooden Material names.

--- src/main/scala/k2b6s9j/boatcraft/compatibility/vanilla/materials/wood/package.scala
+++ src/main/scala/k2b6s9j/boatcraft/compatibility/vanilla/materials/wood/package.scala
@@ -2,15 +2,15 @@ package k2b6s9j.boatcraft.compatibility.vanilla.materials
 
 package object wood
 {
-    object Oak extends MaterialWood(0, "Oak")
+    object Oak extends MaterialWood(0, "Oak Wood")
     
-    object Spruce extends MaterialWood(1, "Spruce")
+    object Spruce extends MaterialWood(1, "Spruce Wood")
     
-    object Birch extends MaterialWood(2, "Birch")
+    object Birch extends MaterialWood(2, "Birch Wood")
     
-    object Jungle extends MaterialWood(3, "Jungle")
+    object Jungle extends MaterialWood(3, "Jungle Wood")
     
-    object Acacia extends MaterialWood(4, "Acacia")
+    object Acacia extends MaterialWood(4, "Acacia Wood")
     
-    object DarkOak extends MaterialWood(5, "Dark Oak")
+    object DarkOak extends MaterialWood(5, "Dark Oak Wood")
 }
