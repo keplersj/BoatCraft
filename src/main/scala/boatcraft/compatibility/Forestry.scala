dbfd64c80811e70d44b45e1af1137f5694122151
viliml <vilim.lendvaj@sk.t-com.hr>
2014-05-28 12:54:00 +0200

    Update Forestry.scala

--- src/main/scala/boatcraft/compatibility/Forestry.scala
+++ src/main/scala/boatcraft/compatibility/Forestry.scala
@@ -1,9 +1,11 @@
 package boatcraft.compatibility
 
+import cpw.mods.fml.common.Optional
 import boatcraft.api.modifiers.Material
 import boatcraft.compatibility.forestry.modifiers.materials.wood.MaterialForestryWood
 
-object Forestry extends CompatModule {
+object Forestry extends CompatModule("Forestry") {
 	
+	@Optional.Method(modid = "Forestry")
 	override def getMaterials = MaterialForestryWood.values
 }
