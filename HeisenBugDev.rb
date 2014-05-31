fd4371a70f648c4914e3aeffe5399c1429a804e8
Vilim <vilim.lendvaj@sk.t-com.hr>
2014-05-31 15:48:51 +0200

    Back

--- HeisenBugDev.rb
+++ HeisenBugDev.rb
@@ -1,18 +1,22 @@
 require 'base64'
 require 'json'
 require 'net/http'
- 
+
 manifest = JSON.parse(IO.read('build/libs/manifest.json'))
- 
+
 req = Net::HTTP::Post.new('/builds.json', 'Content-Type' => 'application/json')
 req['X-User-Email'] = ENV['HBD_EMAIL_PROD']
 req['X-User-Token'] = ENV['HBD_TOKEN_PROD']
 req.body = manifest.to_json
- 
+
 response = Net::HTTP.new('heisenbugdev.com').start do |http|
-  http.request(req)
+    http.request(req)
 end
 
 puts response.body['upload_path']
 puts response.body
 puts response
+/*
+response = Net::HTTP.new('hesinebugdev.com' + upload_path).start do |http|
+    
+end*/
