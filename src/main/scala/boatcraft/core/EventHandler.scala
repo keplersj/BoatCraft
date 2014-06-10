a9b94244aa453cd800e0d3834e2f46ffbf68d719
Vilim <vilim.lendvaj@sk.t-com.hr>
2014-06-10 10:13:21 +0200

    Fix #235

--- src/main/scala/boatcraft/core/EventHandler.scala
+++ src/main/scala/boatcraft/core/EventHandler.scala
@@ -4,6 +4,7 @@ import boatcraft.api.boat.EntityCustomBoat
 import boatcraft.api.modifiers.ExtendedBoat
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing
+import net.minecraftforge.event.entity.living.LivingHurtEvent
 
 object EventHandler {
 	
@@ -16,5 +17,16 @@ object EventHandler {
 		
 		if (boat.getExtendedProperties(ExtendedBoat.NAME) == null)
 			boat.registerExtendedProperties(ExtendedBoat.NAME, new ExtendedBoat)
+	}
+	
+	@SubscribeEvent
+	def hurtEntity(event: LivingHurtEvent)
+	{
+		if (event.source.isFireDamage && event.entityLiving.ridingEntity != null)
+			event.entityLiving.ridingEntity match
+		{
+			case boat: EntityCustomBoat if boat.getMaterial isImmuneToFire =>
+				event.ammount = 0
+		}
 	}
 }
