e71c2261d6eb7939b3f5adafd1d735dbe6ca9bc4
Vilim Lendvaj <vilim.lendvaj@sk.t-com.hr>
2014-07-08 14:04:09 +0200

    Summary
    Description

--- src/main/scala/boatcraft/api/modifiers/package-info.java
+++ src/main/scala/boatcraft/api/modifiers/package-info.java
@@ -1,3 +1,4 @@
-@API(owner = "boatcraft:api", provides = "boatcraft:api:traits", apiVersion = "2.0") package boatcraft.api.modifiers;
+@API(owner = "boatcraft:api", provides = "boatcraft:api:traits", apiVersion = "2.0")
+package boatcraft.api.modifiers;
 
 import cpw.mods.fml.common.API;
