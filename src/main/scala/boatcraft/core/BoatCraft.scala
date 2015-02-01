18f06f94a38d1172f3ecfa351e3f33498f9b2cfb
Kepler Sticka-Jones <kbsj269@gmail.com>
2015-02-01 16:17:49 -0700

    NovaMod object.

--- src/main/scala/boatcraft/core/BoatCraft.scala
+++ src/main/scala/boatcraft/core/BoatCraft.scala
@@ -1,45 +1,12 @@
 package boatcraft.core
 
-import org.apache.logging.log4j.Logger
-import boatcraft.api.Registry
-import boatcraft.api.boat.ItemCustomBoat
-import boatcraft.compatibility
-import boatcraft.core.modifiers.blocks.Empty
-import cpw.mods.fml.common.Mod
-import cpw.mods.fml.common.SidedProxy
-import cpw.mods.fml.common.event.FMLInitializationEvent
-import cpw.mods.fml.common.event.FMLPostInitializationEvent
-import cpw.mods.fml.common.event.FMLPreInitializationEvent
-import cpw.mods.fml.common.network.NetworkRegistry
-import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper
-import cpw.mods.fml.common.registry.GameRegistry
-import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.common.config.Configuration
-import net.minecraft.init.Items
-import net.minecraft.item.ItemStack
-import net.minecraftforge.oredict.OreDictionary
+import nova.core.loader.{Loadable, NovaMod}
 
-@Mod(modid = "boatcraft",
-	name = "BoatCraft",
-	version = "2.0",
-	modLanguage = "scala",
-	dependencies = "after:IronChest;after:Thaumcraft;after:IC2;after:BuildCraft|Factory;" +
-				"after:Forestry;after:NotEnoughItems;after:CoFHAPI|energy")
-object BoatCraft {
-	@SidedProxy(modId = "boatcraft",
-		clientSide = "boatcraft.core.Proxy$ClientProxy",
-		serverSide = "boatcraft.core.Proxy$CommonProxy")
-	var proxy: Proxy.CommonProxy = null
+@NovaMod(id = "boatcraft", name = "BoatCraft", version = "3.0.0", novaVersion = "0.1.0")
+object BoatCraft extends Loadable {
 
-	private[boatcraft] var config: Configuration = null
-
-	var channel: SimpleNetworkWrapper = null
-
-	private[boatcraft] var log: Logger = null
-
-	@Mod.EventHandler
-	def preInit(event: FMLPreInitializationEvent) {
-		
+	def preInit() = {
+		/*
 		log = event.getModLog
 		
 		config = new Configuration(event getSuggestedConfigurationFile)
@@ -50,9 +17,9 @@ object BoatCraft {
 		
 		NetworkRegistry.INSTANCE.registerGuiHandler("boatcraft", GUIHandler)
 		MinecraftForge.EVENT_BUS register EventHandler
-		
-		printModInfo
-		
+		*/
+		printModInfo()
+		/*
 		compatibility preInit event
 		
 		Registry register Empty
@@ -60,11 +27,11 @@ object BoatCraft {
 		compatibility registerModifiers
 		
 		GameRegistry registerItem(ItemCustomBoat, "customBoat")
+		*/
 	}
 
-	@Mod.EventHandler
-	def init(event: FMLInitializationEvent) {
-		
+	def init() = {
+		/*
 		proxy registerEntities
 
 		proxy registerRenders
@@ -74,17 +41,19 @@ object BoatCraft {
 				new ItemStack(ItemCustomBoat, 1, OreDictionary.WILDCARD_VALUE))
 
 		compatibility init event
+		*/
 	}
 
-	@Mod.EventHandler
-	def postInit(event: FMLPostInitializationEvent) {
-		compatibility postInit event
+	def postInit() = {
+		//compatibility postInit event
 	}
 
-	private def printModInfo {
+	private def printModInfo() {
+		/*
 		log info "BoatCraft"
 		log info "Copyright Kepler Sticka-Jones 2013-2014"
 		log info "http://k2b6s9j.com/projects/minecraft/BoatCraft"
+		*/
 	}
 }
 
